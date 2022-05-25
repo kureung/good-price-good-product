675ff2b7c7801f44fbe9beea2d6021977296b0e8
kureungkureung <terry9611@naver.com>
2022-05-25 23:23:16 +0900

    :tada: 프로젝트 생성 (#1)

---
+++ src/main/java/kr/co/gpgp/GpgpApplication.java
@@ -0,0 +1,13 @@
+package kr.co.gpgp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpgpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GpgpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/gpgp/GpgpApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.gpgp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpgpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
