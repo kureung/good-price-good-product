ff3c9824e8d1f1480012f722b4b04a819b9f83fc
kureung <89060425+kureung@users.noreply.github.com>
2022-05-27 00:35:36 +0900

    refactor: Item 엔티티 수정 (#13)

--- src/test/java/kr/co/gpgp/domain/entity/item/ItemTest.java
+++ src/test/java/kr/co/gpgp/domain/entity/item/ItemTest.java
@@ -20,10 +20,10 @@ public class ItemTest {
         "1, 1, 0"
     })
     void 상품주문시_재고수량이_낮아진다(int stockQuantity, int quantity, int result) {
-        ItemInfo info = ItemInfo.create("name", 10, "123", LocalDate.now());
-        Item item = Item.create(1000, stockQuantity, "www.naver.com", info);
+        ItemInfo info = ItemInfo.of("name", 10, "123", LocalDate.now(), "www.naver.com");
+        Item item = Item.of(1000, stockQuantity, info);
 
-        item.removeStock(quantity);
+        item.minusStock(quantity);
 
         assertThat(item.getStockQuantity()).isEqualTo(result);
     }
@@ -36,10 +36,10 @@ public class ItemTest {
         "0, 1"
     })
     void 상품주문시_재고가_부족하면_예외가_발생한다(int stockQuantity, int quantity) {
-        ItemInfo info = ItemInfo.create("name", 10, "123", LocalDate.now());
-        Item item = Item.create(1000, stockQuantity, "www.naver.com", info);
+        ItemInfo info = ItemInfo.of("name", 10, "123", LocalDate.now(), "www.naver.com");
+        Item item = Item.of(1000, stockQuantity, info);
 
-        assertThatThrownBy(() -> item.removeStock(quantity))
+        assertThatThrownBy(() -> item.minusStock(quantity))
             .isInstanceOf(IllegalStateException.class)
             .hasMessage("재고가 부족합니다.");
     }
@@ -52,10 +52,10 @@ public class ItemTest {
         "1, 1, 2"
     })
     void 재고_추가시_재고수량이_높아진다(int stockQuantity, int quantity, int result) {
-        ItemInfo info = ItemInfo.create("name", 10, "123", LocalDate.now());
-        Item item = Item.create(1000, stockQuantity, "www.naver.com", info);
+        ItemInfo info = ItemInfo.of("name", 10, "123", LocalDate.now(), "www.naver.com");
+        Item item = Item.of(1000, stockQuantity, info);
 
-        item.addStock(quantity);
+        item.plusStock(quantity);
 
         assertThat(item.getStockQuantity()).isEqualTo(result);
     }
@@ -67,11 +67,11 @@ public class ItemTest {
     })
     void 재고_추가시_재고수량이_1억개를_넘어가면_예외가_발생한다(int stockQuantity, int quantity) {
 
-        ItemInfo info = ItemInfo.create("name", 10, "123", LocalDate.now());
-        Item item = Item.create(1000, stockQuantity, "www.naver.com", info);
+        ItemInfo info = ItemInfo.of("name", 10, "123", LocalDate.now(), "www.naver.com");
+        Item item = Item.of(1000, stockQuantity, info);
 
         assertThatThrownBy(() ->
-            item.addStock(quantity))
+            item.plusStock(quantity))
             .isInstanceOf(IllegalStateException.class)
             .hasMessage("재고는 1억개를 넘을 수 없습니다.");
     }
@@ -79,46 +79,59 @@ public class ItemTest {
     @ParameterizedTest
     @MethodSource("updateItem")
     void name(Item item, Item newItem) {
-        item.update(newItem);
+        item.update(newItem.getPrice(),newItem.getStockQuantity(),newItem.getInfo());
         assertThat(item)
             .usingRecursiveComparison()
             .isEqualTo(newItem);
     }
 
     private static Stream<Arguments> updateItem() {
-        ItemInfo info = ItemInfo.create("name", 10, "123123", LocalDate.now());
-        Item item = Item.create(1000, 10, "https://www.naver.com/", info);
-
         return Stream.of(
             Arguments.of(
-                Item.create(
+                Item.of(
                     1100,
                     11,
-                    "https://www.google.co.kr/",
-                    ItemInfo.create("item1", 11, "321321", LocalDate.now().minusDays(1))
+                    ItemInfo.of(
+                        "item1",
+                        11,
+                        "321321",
+                        LocalDate.now().minusDays(1),
+                        "www.google.co.kr")
                 ),
 
-                Item.create(
+                Item.of(
                     1000,
                     10,
-                    "https://www.naver.com/",
-                    ItemInfo.create("item2", 12, "123123", LocalDate.now())
+                    ItemInfo.of(
+                        "item2",
+                        12,
+                        "123123",
+                        LocalDate.now(),
+                        "www.naver.com")
                 )
             ),
 
             Arguments.of(
-                Item.create(
+                Item.of(
                     1000,
                     10,
-                    "https://www.naver.com/",
-                    ItemInfo.create("item2", 12, "123123", LocalDate.now())
+                    ItemInfo.of(
+                        "item2",
+                        12,
+                        "123123",
+                        LocalDate.now(),
+                        "www.naver.com")
                 ),
 
-                Item.create(
+                Item.of(
                     1100,
                     11,
-                    "https://www.google.co.kr/",
-                    ItemInfo.create("item1", 11, "321321", LocalDate.now().minusDays(1))
+                    ItemInfo.of(
+                        "item1",
+                        11,
+                        "321321",
+                        LocalDate.now().minusDays(1),
+                        "www.google.co.kr")
                 )
             )
         );
