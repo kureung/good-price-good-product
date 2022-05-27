563e26a13d9cba41c192a8ebaa8fa88ffebd6834
oiNeh <kgh22522252@gmail.com>
2022-05-27 16:08:30 +0900

    test: 배송 엔티티 추가(#5)

---
+++ src/test/java/kr/co/gpgp/domain/delivery/entity/DeliveryTest.java
@@ -0,0 +1,33 @@
+package kr.co.gpgp.domain.delivery.entity;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class DeliveryTest {
+
+    @Test
+    void 이미_완료된_배송에서_다음상태로변경하면_예외가_발생한다() {
+        assertThatThrownBy(() -> {
+                Delivery delivery = new Delivery();
+                delivery.next();
+                delivery.next();
+                delivery.next();
+                delivery.next();
+                delivery.next();
+                delivery.next();
+            }
+        ).hasMessage("이미 완료된 배송입니다.");
+
+    }
+    @Test
+    void 배송준비단계에서_다음상태로변경하면_예외는_발생하지_않는다() {
+        Delivery delivery = new Delivery();
+        delivery.next();
+        delivery.next();
+        assertThat(delivery)
+            .isNotInstanceOf(ArrayIndexOutOfBoundsException.class);
+
+    }
+
+}
