d7571d8bac415d20a6337f88d72ec482b2c5f907
kureungkureung <terry9611@naver.com>
2022-05-27 18:18:01 +0900

    feat: ItemServiceTest 추가

--- src/test/java/kr/co/gpgp/domain/item/service/ItemServiceTest.java
+++ src/test/java/kr/co/gpgp/domain/item/service/ItemServiceTest.java
@@ -1,7 +1,237 @@
 package kr.co.gpgp.domain.item.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.LocalDate;
+import java.util.stream.Stream;
+import kr.co.gpgp.domain.item.entity.Item;
+import kr.co.gpgp.domain.item.entity.ItemInfo;
+import kr.co.gpgp.domain.item.repository.ItemRepository;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
 class ItemServiceTest {
 
+    @Autowired
+    private ItemRepository itemRepository;
+
+    @Autowired
+    private ItemService itemService;
+
+    @ParameterizedTest
+    @MethodSource("toSaveProvideItem")
+    void 상품_등록_테스트(Item item) {
+
+        // when
+        Long itemId = itemService.save(item);
+        Item findItem = itemRepository.findById(itemId)
+            .orElseThrow(() -> new IllegalStateException("존재하지 않는 회원입니다."));
+
+        // then
+        assertThat(item).isEqualTo(findItem);
+    }
+
+    private static Stream<Arguments> toSaveProvideItem() {
+        return Stream.of(
+            Arguments.of(
+                Item.of(
+                    1000,
+                    100,
+                    ItemInfo.of(
+                        "item1",
+                        500,
+                        "123",
+                        LocalDate.now(),
+                        "www.naver.com"
+                    ))
+            ),
+
+            Arguments.of(
+                Item.of(
+                    2000,
+                    300,
+                    ItemInfo.of(
+                        "item2",
+                        600,
+                        "456",
+                        LocalDate.now().minusMonths(1),
+                        "www.google.co.kr"
+                    ))
+            ),
+
+            Arguments.of(
+                Item.of(
+                    3000,
+                    200,
+                    ItemInfo.of(
+                        "item3",
+                        700,
+                        "789",
+                        LocalDate.now().minusDays(2),
+                        "www.daum.net"
+                    )
+                )
+            )
+        );
+    }
+
+
+    @ParameterizedTest
+    @MethodSource("toFindOneProvideItem")
+    void 상품_조회_테스트(Item item) {
+
+        // given
+        Item savedItem = itemRepository.save(item);
+        Long itemId = savedItem.getId();
+
+        // when
+        Item findItem = itemService.findOne(itemId);
+
+        // then
+        assertThat(item).usingRecursiveComparison()
+            .ignoringFields("id")
+            .isEqualTo(findItem);
+    }
+
+    private static Stream<Arguments> toFindOneProvideItem() {
+        return Stream.of(
+            Arguments.of(
+                Item.of(
+                    1000,
+                    100,
+                    ItemInfo.of(
+                        "item1",
+                        500,
+                        "123",
+                        LocalDate.now(),
+                        "www.naver.com"
+                    ))
+            ),
+
+            Arguments.of(
+                Item.of(
+                    2000,
+                    300,
+                    ItemInfo.of(
+                        "item2",
+                        600,
+                        "456",
+                        LocalDate.now().minusMonths(1),
+                        "www.google.co.kr"
+                    ))
+            ),
+
+            Arguments.of(
+                Item.of(
+                    3000,
+                    200,
+                    ItemInfo.of(
+                        "item3",
+                        700,
+                        "789",
+                        LocalDate.now().minusDays(2),
+                        "www.daum.net"
+                    )
+                )
+            )
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("toUpdateProvideItem")
+    void 상품_수정_테스트(Item item, Item newItem) {
+
+        // given
+        Item savedItem = itemRepository.save(item);
+        Long itemId = savedItem.getId();
+
+        // when
+        itemService.update(itemId, newItem);
+        Item findItem = itemRepository.findById(itemId)
+            .orElseThrow(() -> new IllegalStateException("존재하지 않는 회원입니다."));
+
+        // then
+        assertThat(findItem).usingRecursiveComparison()
+            .ignoringFields("id")
+            .isEqualTo(newItem);
+    }
+
+    private static Stream<Arguments> toUpdateProvideItem() {
+        return Stream.of(
+            Arguments.of(
+                Item.of(
+                    1000,
+                    100,
+                    ItemInfo.of(
+                        "item1",
+                        500,
+                        "123",
+                        LocalDate.now(),
+                        "www.naver.com"
+                    )),
+                Item.of(
+                    2000,
+                    300,
+                    ItemInfo.of(
+                        "item2",
+                        600,
+                        "456",
+                        LocalDate.now().minusMonths(1),
+                        "www.google.co.kr"
+                    ))
+            ),
+
+            Arguments.of(
+                Item.of(
+                    2000,
+                    300,
+                    ItemInfo.of(
+                        "item2",
+                        600,
+                        "456",
+                        LocalDate.now().minusMonths(1),
+                        "www.google.co.kr"
+                    )),
+                Item.of(
+                    1000,
+                    100,
+                    ItemInfo.of(
+                        "item1",
+                        500,
+                        "123",
+                        LocalDate.now(),
+                        "www.naver.com"
+                    ))
+            ),
+
+            Arguments.of(
+                Item.of(
+                    3000,
+                    200,
+                    ItemInfo.of(
+                        "item3",
+                        700,
+                        "789",
+                        LocalDate.now().minusDays(2),
+                        "www.daum.net"
+                    )),
+                Item.of(
+                    2000,
+                    300,
+                    ItemInfo.of(
+                        "item2",
+                        600,
+                        "456",
+                        LocalDate.now().minusMonths(1),
+                        "www.google.co.kr"
+                    ))
+                )
+        );
+    }
 }
