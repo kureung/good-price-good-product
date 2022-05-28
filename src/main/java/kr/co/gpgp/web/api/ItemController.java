a02af10166a0381999b71c7851eba177f7d610ad
kureungkureung <terry9611@naver.com>
2022-05-29 03:19:37 +0900

    feat: 상품 컨트롤러 추가

--- src/main/java/kr/co/gpgp/web/api/ItemController.java
+++ src/main/java/kr/co/gpgp/web/api/ItemController.java
@@ -1,11 +1,55 @@
 package kr.co.gpgp.web.api;
 
+import java.net.URI;
+import javax.validation.Valid;
+import kr.co.gpgp.domain.item.dto.ItemDtoRequest;
+import kr.co.gpgp.domain.item.dto.ItemDtoResponse;
+import kr.co.gpgp.domain.item.entity.Item;
+import kr.co.gpgp.domain.item.service.ItemService;
+import kr.co.gpgp.domain.item.service.dto.ItemDtoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
-@RequestMapping("/items")
+@RequestMapping("/api/items")
 @RequiredArgsConstructor
 public class ItemController {
+
+    private final ItemService itemService;
+    private final ItemDtoService itemDtoService;
+
+    @PostMapping
+    public ResponseEntity<ItemDtoResponse> register(
+        @Valid ItemDtoRequest request,
+        BindingResult result) {
+
+        Item item = itemDtoService.dtoConversionItem(request);
+        Long itemId = itemService.save(item);
+        Item findItem = itemService.findOne(itemId);
+        ItemDtoResponse response = itemDtoService.itemConversionDto(findItem);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+            .path("/{id}")
+            .buildAndExpand(findItem.getId())
+            .toUri();
+        return ResponseEntity.created(location)
+            .body(response);
+    }
+
+    @GetMapping("/{itemId}")
+    public ResponseEntity<ItemDtoResponse> findOneItem(
+        @PathVariable Long id) {
+
+        Item findItem = itemService.findOne(id);
+        ItemDtoResponse response = itemDtoService.itemConversionDto(findItem);
+        return ResponseEntity.ok(response);
+    }
+
 }
