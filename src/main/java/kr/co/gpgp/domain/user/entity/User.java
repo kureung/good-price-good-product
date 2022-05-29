4879bc4c6659e9c647b278196b212eaa37333d28
oiNeh <kgh22522252@gmail.com>
2022-05-29 21:47:03 +0900

    feat: Seller 와 연관관계가 추가되었다. (#31)
    1:1 - 양방향이다.

--- src/main/java/kr/co/gpgp/domain/user/entity/User.java
+++ src/main/java/kr/co/gpgp/domain/user/entity/User.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import lombok.Builder;
 import lombok.Getter;
@@ -19,6 +20,9 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @OneToOne
+    private Seller seller;
 
     private String name;
     private String email;
