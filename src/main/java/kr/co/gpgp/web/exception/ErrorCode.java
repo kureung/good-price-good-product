2e6f865f0813d8dadab7b31c0ef84f459b241c10
kureungkureung <terry9611@naver.com>
2022-06-01 04:22:47 +0900

    refactor: 에러코드 수정

--- src/main/java/kr/co/gpgp/web/exception/ErrorCode.java
+++ src/main/java/kr/co/gpgp/web/exception/ErrorCode.java
@@ -1,38 +1,26 @@
 package kr.co.gpgp.web.exception;
 
-import java.util.Arrays;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 public enum ErrorCode {
 
     // Common
-    INVALID_INPUT_VALUE(400, "C001", "적절하지 않은 요청 값입니다."),
-    INTERNAL_SERVER_ERROR(500, "C002", "서버에 문제가 생겼습니다."),
-    METHOD_NOT_ALLOWED(405, "C003", "적절하지 않은 HTTP 메소드입니다."),
-    INVALID_TYPE_VALUE(400, "C005", "요청 값의 타입이 잘못되었습니다."),
+    INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "적절하지 않은 요청 값입니다."),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,  "서버에 문제가 생겼습니다."),
+    METHOD_NOT_ALLOWED(HttpStatus.NOT_FOUND,  "적절하지 않은 HTTP 메소드입니다."),
+    INVALID_TYPE_VALUE(HttpStatus.BAD_REQUEST,  "요청 값의 타입이 잘못되었습니다."),
 
     // Item
-    STOCK_OUT_OF_RANGE(400, "I001", "재고가 부족합니다."),
-
-
-
+    STOCK_OUT_OF_RANGE(HttpStatus.BAD_REQUEST,  "재고 범위를 벗어났습니다."),
     ;
 
-    private final int status;
-    private final String code;
+    private final HttpStatus status;
     private final String message;
 
-    ErrorCode(int status, String code, String message) {
+    ErrorCode(HttpStatus status, String message) {
         this.status = status;
-        this.code = code;
         this.message = message;
-    }
-
-    public static ErrorCode fromMessage(String message) {
-        return Arrays.stream(ErrorCode.values())
-            .filter(errorCode -> errorCode.getMessage().equals(message))
-            .findFirst()
-            .orElse(null);
     }
 }
