e4513ac6fa1e7228ec544089245c727100c122be
oiNeh <kgh22522252@gmail.com>
2022-06-01 01:17:28 +0900

    feat: jaspt 추가로 인해 수정

--- src/main/java/kr/co/gpgp/GpgpApplication.java
+++ src/main/java/kr/co/gpgp/GpgpApplication.java
@@ -1,8 +1,13 @@
 package kr.co.gpgp;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableEncryptableProperties
 @SpringBootApplication
 public class GpgpApplication {
 
