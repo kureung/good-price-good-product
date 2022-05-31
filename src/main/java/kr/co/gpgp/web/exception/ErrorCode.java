829625c15998f9e7f8032ab6b8e3282ced07434e
kureungkureung <terry9611@naver.com>
2022-05-31 16:43:10 +0900

    feat: 에러코드 추가

---
+++ src/main/java/kr/co/gpgp/web/exception/ErrorCode.java
@@ -0,0 +1,24 @@
+package kr.co.gpgp.web.exception;
+
+import lombok.Getter;
+
+@Getter
+public enum ErrorCode {
+
+    // Common
+    INVALID_INPUT_VALUE(400, "C001", "적절하지 않은 요청 값입니다."),
+    INTERNAL_SERVER_ERROR(500, "C002" , "서버에 문제가 생겼습니다."),
+    METHOD_NOT_ALLOWED(405, "C003", "적절하지 않은 HTTP 메소드입니다."),
+    INVALID_TYPE_VALUE(400, "C005", "요청 값의 타입이 잘못되었습니다."),
+    ;
+
+    private final int status;
+    private final String code;
+    private final String message;
+
+    ErrorCode(int status, String code, String message) {
+        this.status = status;
+        this.code = code;
+        this.message = message;
+    }
+}
