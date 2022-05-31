81ac4cffe3430b0b114946c474e751813b78312e
oiNeh <kgh22522252@gmail.com>
2022-06-01 00:49:58 +0900

    feat: oauth2 추가로 인한 수정(#42)
    pw 을 빼버림

--- src/test/java/kr/co/gpgp/domain/user/repository/UserRepositoryTest.java
+++ src/test/java/kr/co/gpgp/domain/user/repository/UserRepositoryTest.java
@@ -16,26 +16,26 @@ public class UserRepositoryTest {
     void login(){
         User user = User.builder()
             .name("kang")
-            .pw("1q2w3e4r!")
+            //.pw("1q2w3e4r!")
             .email("kgh2252@naver.com")
             .build();
 
         userRepository.save(user);
 
-        System.out.println(userRepository.findByEmailAndPw("kgh2252@naver.com","1q2w3e4r!").getName());
+//        System.out.println(userRepository.findByEmailAndPw("kgh2252@naver.com","1q2w3e4r!").getName());
     }
     @Test
     void 로그인실패시예외(){
         User user = User.builder()
             .name("kang")
-            .pw("1q2w3e4r!")
+            //.pw("1q2w3e4r!")
             .email("kgh2252@naver.com")
             .build();
 
         userRepository.save(user);
 
-        assertThatThrownBy(() -> userRepository.findByEmailAndPw("kgh2252@naver.com","1q2we4r!").getId())
-            .isInstanceOf(NullPointerException.class);
+//        assertThatThrownBy(() -> userRepository.findByEmailAndPw("kgh2252@naver.com","1q2we4r!").getId())
+//            .isInstanceOf(NullPointerException.class);
 
     }
 
@@ -43,13 +43,13 @@ public class UserRepositoryTest {
     void 이메일_중복체크(){
         User user = User.builder()
             .name("kang")
-            .pw("1q2w3e4r!")
+            //.pw("1q2w3e4r!")
             .email("kgh2252@naver.com")
             .build();
 
         userRepository.save(user);
 
-        assertThat(userRepository.findByEmail("kgh2252@naver.com").getId())
+        assertThat(userRepository.findByEmail("kgh2252@naver.com"))
             .isNotInstanceOf(IllegalArgumentException.class);
     }
 
