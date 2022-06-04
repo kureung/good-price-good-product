7653fdd6e03386867de8e5d0ffdec0879ba6a50f
oiNeh <kgh22522252@gmail.com>
2022-06-05 06:22:36 +0900

    feat: address 생성삭제변경 추가(#68)

--- src/main/java/kr/co/gpgp/domain/address/service/AddressService.java
+++ src/main/java/kr/co/gpgp/domain/address/service/AddressService.java
@@ -3,43 +3,59 @@ package kr.co.gpgp.domain.address.service;
 import kr.co.gpgp.domain.address.entity.Address;
 import kr.co.gpgp.domain.address.repository.AddressRepository;
 import kr.co.gpgp.domain.user.entity.User;
+import kr.co.gpgp.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service
+@RequiredArgsConstructor
 public class AddressService {
 
+    @Autowired
     private AddressRepository addressRepository;
+    @Autowired
+    private UserRepository userRepository;
 
     // 1 . 주소는 생성할수 있다. ㅕ
-    public void create(User user, String roadName, String zipCode, String name, String detailed) {
-        /*
-            유효성 검사
-         */
-        addressRepository.save(
-                Address.of(user, roadName, zipCode, name, detailed)
-        );
-    }
-
-    public void create(Address address) {
-        addressRepository.save(
-                Address.of(address.getUser(), address.getRoadName(),
-                        address.getZipCode(), address.getName(), address.getDetailed())
-        );
+    public Address create(User user, String roadName, String zipCode, String name, String detailed) {
+        return addressRepository.save(
+                Address.of(user,
+                        roadName,
+                        zipCode,
+                        name,
+                        detailed));
     }
 
     // 2.  주소는 삭제할수 있다.
-    public void delete(Long user_id,Long address_id) {
+    @Transactional
+    public void delete(Long addressId) {
 
-        addressRepository.findById(user_id)
-                        .orElseThrow(() -> new IllegalArgumentException("[AddressService]-해당 회원을 찾을 수 없습니다."));
+        Address address = addressRepository.findById(addressId)
+                .orElseThrow(() -> new IllegalArgumentException("[AddressService] 주소 ID 가 없어 주소를 삭제할수 없습니다."));
 
-        addressRepository.findById(user_id);
-        addressRepository.findById(address_id);
-
-        addressRepository.deleteById(address_id);
+        addressRepository.delete(address);
     }
+
 
     // 3. 주소는 수정할수 있다.
-    public void update(Long id,Address address) {
-
+    public void updateRoadName(Address updateAddress, String roadName) {
+        updateAddress.updateRoadName(roadName);
     }
+
+    public void updateZipCode(Address updateAddress, String zipCode) {
+        updateAddress.updateZipCode(zipCode);
+    }
+
+    public void updateName(Address updateAddress, String name) {
+        updateAddress.updateName(name);
+    }
+
+    public void updateDetailed(Address updateAddress, String detailed) {
+        updateAddress.updateDetailed(detailed);
+    }
+
 
 }
