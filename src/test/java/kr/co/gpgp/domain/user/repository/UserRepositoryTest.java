9a3517a6b00a854d154b54392a28e5f5c0a94b12
oiNeh <kgh22522252@gmail.com>
2022-06-04 23:44:57 +0900

    chore: oauth2 로그인 변경으로 인한 삭제  (#75)

--- src/test/java/kr/co/gpgp/domain/user/repository/UserRepositoryTest.java
+++ src/test/java/kr/co/gpgp/domain/user/repository/UserRepositoryTest.java
@@ -12,46 +12,4 @@ public class UserRepositoryTest {
     @Autowired
     UserRepository userRepository;
 
-    @Test
-    void login(){
-        User user = User.builder()
-            .name("kang")
-            //.pw("1q2w3e4r!")
-            .email("kgh2252@naver.com")
-            .build();
-
-        userRepository.save(user);
-
-//        System.out.println(userRepository.findByEmailAndPw("kgh2252@naver.com","1q2w3e4r!").getName());
-    }
-    @Test
-    void 로그인실패시예외(){
-        User user = User.builder()
-            .name("kang")
-            //.pw("1q2w3e4r!")
-            .email("kgh2252@naver.com")
-            .build();
-
-        userRepository.save(user);
-
-//        assertThatThrownBy(() -> userRepository.findByEmailAndPw("kgh2252@naver.com","1q2we4r!").getId())
-//            .isInstanceOf(NullPointerException.class);
-
-    }
-
-    @Test
-    void 이메일_중복체크(){
-        User user = User.builder()
-            .name("kang")
-            //.pw("1q2w3e4r!")
-            .email("kgh2252@naver.com")
-            .build();
-
-        userRepository.save(user);
-
-        assertThat(userRepository.findByEmail("kgh2252@naver.com"))
-            .isNotInstanceOf(IllegalArgumentException.class);
-    }
-
-
 }
