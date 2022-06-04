d9301ebc9367c427e9d9e3e23314a92d6daa2b91
oiNeh <kgh22522252@gmail.com>
2022-06-04 23:44:26 +0900

    chore: import 추가 (#75)

--- src/test/java/kr/co/gpgp/domain/delivery/entity/RequirementTest.java
+++ src/test/java/kr/co/gpgp/domain/delivery/entity/RequirementTest.java
@@ -1,15 +1,17 @@
 package kr.co.gpgp.domain.delivery.entity;
 
 import static org.assertj.core.api.Assertions.*;
+
+import kr.co.gpgp.domain.requirement.entity.Requirement;
 import org.junit.jupiter.api.Test;
 
 public class RequirementTest {
 
     @Test
-    void 요청사항_메시지가_18자를_넘어_예외가_발생했습니다(){
+    void 요청사항_메시지가_18자를_넘어_예외가_발생했습니다() {
         assertThatThrownBy(() -> new Requirement("Hellellpellpellpellpellpellpellpellpellpellpellpellpellpp"))
-            .isInstanceOf(IllegalArgumentException.class)
-            .hasMessage("요청사항은 18자를 넘을수 없습니다.");
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("요청사항은 18자를 넘을수 없습니다.");
     }
 
 }
