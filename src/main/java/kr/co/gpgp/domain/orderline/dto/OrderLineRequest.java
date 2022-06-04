37ec96701a7fc15146556518ddefa7e19aa1b717
kureungkureung <terry9611@naver.com>
2022-06-05 01:50:25 +0900

    refactor: 생성자를 빌더 패턴으로 수정

--- src/main/java/kr/co/gpgp/domain/orderline/dto/OrderLineRequest.java
+++ src/main/java/kr/co/gpgp/domain/orderline/dto/OrderLineRequest.java
@@ -1,11 +1,17 @@
 package kr.co.gpgp.domain.orderline.dto;
 
+import static lombok.AccessLevel.PRIVATE;
+
 import javax.validation.constraints.NotBlank;
-import lombok.Data;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.Range;
 
-@Data
+@Getter
+@RequiredArgsConstructor(access = PRIVATE)
+@Builder
 public class OrderLineRequest {
 
     @NotBlank
