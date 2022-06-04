a47b850d327d8961dc6bb3767107b24164d40150
kureungkureung <terry9611@naver.com>
2022-06-05 01:38:51 +0900

    feat: 주문상품 응답 dto에서bean validation 추가

--- src/main/java/kr/co/gpgp/domain/orderline/dto/OrderLineResponse.java
+++ src/main/java/kr/co/gpgp/domain/orderline/dto/OrderLineResponse.java
@@ -1,13 +1,26 @@
 package kr.co.gpgp.domain.orderline.dto;
 
+import javax.validation.constraints.NotBlank;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.Range;
 
 @Data
 public class OrderLineResponse {
 
+
+    @NotBlank
+    @Length(max = 30)
     private final String itemName;
+
+    @NotBlank
+    @Length(max = 30)
     private final String itemCode;
+
+    @Range(min = 0, max = 1_000_000_000)
     private final int itemPrice;
+
+    @Range(min = 0, max = 10_000)
     private final int itemQuantity;
 
 }
