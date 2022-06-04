1ac88da4f8c2f64232a359b63a8debd3f002aaf2
oiNeh <kgh22522252@gmail.com>
2022-06-04 22:12:58 +0900

    feat: 주소 서비스 추가 (#66)

--- src/main/java/kr/co/gpgp/domain/address/service/AddressService.java
+++ src/main/java/kr/co/gpgp/domain/address/service/AddressService.java
@@ -1,5 +1,45 @@
 package kr.co.gpgp.domain.address.service;
 
+import kr.co.gpgp.domain.address.entity.Address;
+import kr.co.gpgp.domain.address.repository.AddressRepository;
+import kr.co.gpgp.domain.user.entity.User;
+
 public class AddressService {
+
+    private AddressRepository addressRepository;
+
+    // 1 . 주소는 생성할수 있다. ㅕ
+    public void create(User user, String roadName, String zipCode, String name, String detailed) {
+        /*
+            유효성 검사
+         */
+        addressRepository.save(
+                Address.of(user, roadName, zipCode, name, detailed)
+        );
+    }
+
+    public void create(Address address) {
+        addressRepository.save(
+                Address.of(address.getUser(), address.getRoadName(),
+                        address.getZipCode(), address.getName(), address.getDetailed())
+        );
+    }
+
+    // 2.  주소는 삭제할수 있다.
+    public void delete(Long user_id,Long address_id) {
+
+        addressRepository.findById(user_id)
+                        .orElseThrow(() -> new IllegalArgumentException("[AddressService]-해당 회원을 찾을 수 없습니다."));
+
+        addressRepository.findById(user_id);
+        addressRepository.findById(address_id);
+
+        addressRepository.deleteById(address_id);
+    }
+
+    // 3. 주소는 수정할수 있다.
+    public void update(Long id,Address address) {
+
+    }
 
 }
