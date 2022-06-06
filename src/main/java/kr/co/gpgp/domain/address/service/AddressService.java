29a0249bceaa8e89b0d96be1eb69cf055c5f651c
oiNeh <kgh22522252@gmail.com>
2022-06-06 23:35:06 +0900

    refactor: 주소 서비스 변경(#68)

--- src/main/java/kr/co/gpgp/domain/address/service/AddressService.java
+++ src/main/java/kr/co/gpgp/domain/address/service/AddressService.java
@@ -1,61 +1,69 @@
 package kr.co.gpgp.domain.address.service;
 
+import java.util.List;
+import kr.co.gpgp.domain.address.dto.AddressRequest;
+import kr.co.gpgp.domain.address.dto.AddressResponse;
 import kr.co.gpgp.domain.address.entity.Address;
 import kr.co.gpgp.domain.address.repository.AddressRepository;
 import kr.co.gpgp.domain.user.entity.User;
 import kr.co.gpgp.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class AddressService {
 
-    @Autowired
-    private AddressRepository addressRepository;
-    @Autowired
-    private UserRepository userRepository;
+
+    private final AddressRepository addressRepository;
+
+    private final UserRepository userRepository;
 
     // 1 . 주소는 생성할수 있다. ㅕ
-    public Address create(User user, String roadName, String zipCode, String name, String detailed) {
-        return addressRepository.save(
+    public Address create(Long userId, AddressResponse addressResponse) {
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("user ID를 조회할수 없어 주소를 생성할수 없습니다."));
+
+        Address address = addressRepository.save(
                 Address.of(user,
-                        roadName,
-                        zipCode,
-                        name,
-                        detailed));
+                        addressResponse.getRoadName(),
+                        addressResponse.getZipCode(),
+                        addressResponse.getName(),
+                        addressResponse.getDetailed()));
+
+        return address;
     }
 
     // 2.  주소는 삭제할수 있다.
-    @Transactional
     public void delete(Long addressId) {
 
         Address address = addressRepository.findById(addressId)
-                .orElseThrow(() -> new IllegalArgumentException("[AddressService] 주소 ID 가 없어 주소를 삭제할수 없습니다."));
+                .orElseThrow(() -> new IllegalArgumentException("address ID 가 없어 주소를 삭제할수 없습니다."));
 
         addressRepository.delete(address);
     }
 
 
     // 3. 주소는 수정할수 있다.
-    public void updateRoadName(Address updateAddress, String roadName) {
-        updateAddress.updateRoadName(roadName);
+    public void update(Long addressBeforeId,  AddressResponse addressResponse) {
+
+        Address addressBefore = addressRepository.findById(addressBeforeId)
+                .orElseThrow(() -> new IllegalArgumentException("변경할 Address ID 값을 조회할수 없어 변경을 할수 없습니다."));
+
+        addressBefore.update(addressResponse.toEntity(addressBefore.getUser()));
     }
 
-    public void updateZipCode(Address updateAddress, String zipCode) {
-        updateAddress.updateZipCode(zipCode);
-    }
+    //4. 주소는 조회가 가능하다.
+    public List<Address> select(Long userId) {
 
-    public void updateName(Address updateAddress, String name) {
-        updateAddress.updateName(name);
-    }
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("조회하려는 User ID 값이 조회할수 없습니다."));
 
-    public void updateDetailed(Address updateAddress, String detailed) {
-        updateAddress.updateDetailed(detailed);
-    }
+        return addressRepository.findByUser(userId);
 
+    }
 
 }
