3754bff64a9a234a757947173e8463807ec9f606
oiNeh <kgh22522252@gmail.com>
2022-06-08 06:56:54 +0900

    test: 배송 enum 리펙토링에 맞게 test 코드 수정 (#71)

--- src/test/java/kr/co/gpgp/domain/delivery/entity/DeliveryTest.java
+++ src/test/java/kr/co/gpgp/domain/delivery/entity/DeliveryTest.java
@@ -1,34 +1,53 @@
 package kr.co.gpgp.domain.delivery.entity;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import kr.co.gpgp.domain.address.entity.Address;
+import kr.co.gpgp.domain.delivery.entity.enums.DeliveryStatusImpl;
 import kr.co.gpgp.domain.delivery.exception.DeliveryStatusOverflowException;
+import kr.co.gpgp.domain.requirement.entity.Requirement;
+import kr.co.gpgp.domain.user.entity.Role;
+import kr.co.gpgp.domain.user.entity.User;
 import org.junit.jupiter.api.Test;
+
 
 public class DeliveryTest {
 
+    Requirement requirement = new Requirement("요청사항");
+    User user = User.of("asdf", "kgh2252@naver.com", Role.USER);
+    Address address = Address.of(user, "1234567890", "12345", "12345", "12345");
+
     @Test
     void 이미_완료된_배송에서_다음상태로변경하면_예외가_발생한다() {
+
         assertThatThrownBy(() -> {
-                Delivery delivery = new Delivery();
-                delivery.next();
-                delivery.next();
-                delivery.next();
-                delivery.next();
-                delivery.next();
-                delivery.next();
-            }
-        ).hasMessage("이미 완료된 배송입니다.");
-
+            Delivery delivery = Delivery.of(requirement, address);
+            delivery.next();
+            delivery.next();
+            delivery.next();
+            delivery.next();
+            delivery.next();
+            delivery.next();
+        }).hasMessage("이미 완료된 배송입니다.");
     }
-    @Test
-    void 배송준비단계에서_다음상태로변경하면_예외는_발생하지_않는다() {
-        Delivery delivery = new Delivery();
-        delivery.next();
-        delivery.next();
-        assertThat(delivery)
-            .isNotInstanceOf(DeliveryStatusOverflowException.class);
 
+    @Test
+    void 배송순서가_정상적으로_돌아가면_예외는_발생되지_않는다() {
+        Delivery delivery = Delivery.of(requirement, address);
+
+        assertThat(delivery.getStatus().get()).isEqualTo(DeliveryStatusImpl.ACCEPT.get());
+        delivery.next();
+        assertThat(delivery.getStatus().get()).isEqualTo(DeliveryStatusImpl.INSTRUCT.get());
+        delivery.next();
+        assertThat(delivery.getStatus().get()).isEqualTo(DeliveryStatusImpl.DEPARTURE.get());
+        delivery.next();
+        assertThat(delivery.getStatus().get()).isEqualTo(DeliveryStatusImpl.FINAL_DELIVERY.get());
+        delivery.next();
+        assertThat(delivery.getStatus().get()).isEqualTo(DeliveryStatusImpl.NONE_TRACKING.get());
+
+        assertThat(delivery)
+                .isNotInstanceOf(DeliveryStatusOverflowException.class);
     }
 
 }
