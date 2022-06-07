b1a26b8f1c199a172a24708bde771988f44c0b1d
kureungkureung <terry9611@naver.com>
2022-06-07 21:43:56 +0900

    refactor: 상품 등록 서비스 기능 추가

--- src/main/java/kr/co/gpgp/domain/item/service/ItemCommandService.java
+++ src/main/java/kr/co/gpgp/domain/item/service/ItemCommandService.java
@@ -1,7 +1,10 @@
 package kr.co.gpgp.domain.item.service;
 
+import kr.co.gpgp.domain.item.dto.ItemDtoRequest;
+import kr.co.gpgp.domain.item.dto.ItemDtoResponse;
 import kr.co.gpgp.domain.item.entity.Item;
 import kr.co.gpgp.domain.item.repository.ItemRepository;
+import kr.co.gpgp.domain.item.service.dto.ItemDtoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,6 +16,7 @@ public class ItemCommandService {
 
     private final ItemRepository itemRepository;
     private final ItemFindService itemFindService;
+    private final ItemDtoService itemDtoService;
 
     public Long save(Item item) {
         Item savedItem = itemRepository.save(item);
@@ -22,5 +26,12 @@ public class ItemCommandService {
     public void update(Long itemId, Item item) {
         Item findItem = itemFindService.findOne(itemId);
         findItem.update(item.getPrice(), item.getStockQuantity(), item.getInfo());
+    }
+
+    public ItemDtoResponse register(ItemDtoRequest request) {
+        Item item = itemDtoService.dtoConversionItem(request);
+        Long itemId = save(item);
+        Item findItem = itemFindService.findOne(itemId);
+        return itemDtoService.itemConversionDto(findItem);
     }
 }
