4e64894d98e531b949f6e8412607fafb70ba7e3a
oiNeh <kgh22522252@gmail.com>
2022-06-08 01:04:45 +0900

    feat: 쿼리 메소드 추가  (#68)

--- src/main/java/kr/co/gpgp/domain/address/repository/AddressRepository.java
+++ src/main/java/kr/co/gpgp/domain/address/repository/AddressRepository.java
@@ -1,8 +1,12 @@
 package kr.co.gpgp.domain.address.repository;
 
+import java.util.List;
 import kr.co.gpgp.domain.address.entity.Address;
+import kr.co.gpgp.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AddressRepository extends JpaRepository<Address,Long> {
 
+    List<Address> findByUserId(Long userId);
+;
 }
