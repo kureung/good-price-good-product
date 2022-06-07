1d4e8ed3a1aaf88b497d4ba4fade7b2b1999edb9
oiNeh <kgh22522252@gmail.com>
2022-06-08 04:44:30 +0900

    feat: queryDSL 적용 한 dto (#68)

--- src/main/java/kr/co/gpgp/domain/address/dto/AddressResponse.java
+++ src/main/java/kr/co/gpgp/domain/address/dto/AddressResponse.java
@@ -1,41 +1,43 @@
 package kr.co.gpgp.domain.address.dto;
 
 
+import com.querydsl.core.annotations.QueryProjection;
 import kr.co.gpgp.domain.address.entity.Address;
 import kr.co.gpgp.domain.user.entity.User;
-import lombok.AccessLevel;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
-@Data
-@ToString
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@NoArgsConstructor
 public class AddressResponse {
 
+    private Long id;
     private String roadName;        //도로명
     private String zipCode;         //우편번호
     private String name;            //주소이름(닉네임)
     private String detailed;        //상세주소
 
 
-    private AddressResponse(String roadName, String zipCode, String name, String detailed) {
+    @QueryProjection
+    public AddressResponse(Long id, String roadName, String zipCode, String name, String detailed) {
+        this.id = id;
         this.roadName = roadName;
         this.zipCode = zipCode;
         this.name = name;
         this.detailed = detailed;
     }
 
-    public static AddressResponse of(AddressRequest address) {
-        return new AddressResponse(address.getRoadName(),
+
+    public static AddressResponse of(Address address) {
+        return new AddressResponse(address.getId(),
+                address.getRoadName(),
                 address.getZipCode(),
                 address.getName(),
                 address.getDetailed());
     }
 
     public Address toEntity(User user) {
-
         return Address.of(user, roadName, zipCode, name, detailed);
     }
-
 }
