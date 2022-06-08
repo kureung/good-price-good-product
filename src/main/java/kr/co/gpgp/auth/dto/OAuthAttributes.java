0497a52a57c648d3e5934ae21cfddc0c2006d351
oiNeh <kgh22522252@gmail.com>
2022-06-08 22:34:07 +0900

    feat : oauth2 OAuthAttributes 수정 (#94)

--- src/main/java/kr/co/gpgp/auth/dto/OAuthAttributes.java
+++ src/main/java/kr/co/gpgp/auth/dto/OAuthAttributes.java
@@ -1,20 +1,13 @@
 package kr.co.gpgp.auth.dto;
 
+import java.util.Map;
 import kr.co.gpgp.domain.user.entity.Role;
 import kr.co.gpgp.domain.user.entity.User;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
-import lombok.extern.slf4j.Slf4j;
 
-import java.util.HashMap;
-import java.util.Map;
 
-/**
- * 스프링 시큐리티 OAuth 인증을 위한 속성 객체
- * ofNaver, ofKakao 등 of플랫폼명으로 된 메소드들의 리팩토링 여지가 있음.
- */
-@Slf4j
 @Getter
 @ToString
 public class OAuthAttributes {
@@ -24,33 +17,33 @@ public class OAuthAttributes {
     private String email;
 
     @Builder
-    public OAuthAttributes(Map<String, Object> attributes, String nameAttributeKey, String name, String email){
+    public OAuthAttributes(Map<String, Object> attributes, String nameAttributeKey, String name, String email) {
         this.attributes = attributes;
         this.nameAttributeKey = nameAttributeKey;
         this.name = name;
         this.email = email;
     }
 
-    public static OAuthAttributes of(String userNameAttributeName, Map<String, Object> attributes) {
-
-        log.info("유저이름 :: "+userNameAttributeName);
-        log.info("속성 :: "+attributes);
-
-        return ofKakao( "nickname", attributes);
-
+    public static OAuthAttributes of(Map<String, Object> attributes) {
+        return ofKakao((Map<String, Object>) attributes.get("kakao_account"));
     }
-    private static OAuthAttributes ofKakao( String userNameAttributeName, Map<String, Object> attributes){
-        Map<String, Object> properties = (Map<String, Object>) attributes.get("properties");
-        properties.put("id", attributes.get("id"));
+
+    private static OAuthAttributes ofKakao(Map<String, Object> attributes) {
+        Map<String, Object> map = (Map<String, Object>) attributes.get("profile");
+
+        String name = (String) map.get("nickname");
+        String email = (String) attributes.get("email");
+//        String url = (String) map.get("profile_image_url");
 
         return OAuthAttributes.builder()
-            .name((String) properties.get("nickname"))
-            .email((String) properties.get("email"))
-            .attributes(properties)
-            .nameAttributeKey(userNameAttributeName)
-            .build();
+                .name(name)
+                .email(email)
+                .attributes(map)
+                .nameAttributeKey("nickname")
+                .build();
     }
-    public User toEntity(){
-        return User.of(name,email, Role.USER);
+
+    public User toEntity() {
+        return User.of(name, email, Role.USER);
     }
 }
