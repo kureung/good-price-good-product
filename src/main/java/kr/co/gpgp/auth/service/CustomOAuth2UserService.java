7bad99c69349c4d6d71a150a613c17a7aa6c7075
oiNeh <kgh22522252@gmail.com>
2022-06-08 22:38:20 +0900

    refactor : oauth2 loaduser 코드 수정(#94)

--- src/main/java/kr/co/gpgp/auth/service/CustomOAuth2UserService.java
+++ src/main/java/kr/co/gpgp/auth/service/CustomOAuth2UserService.java
@@ -1,6 +1,8 @@
 package kr.co.gpgp.auth.service;
 
 
+import java.util.Collections;
+import javax.servlet.http.HttpSession;
 import kr.co.gpgp.auth.dto.OAuthAttributes;
 import kr.co.gpgp.auth.dto.SessionUser;
 import kr.co.gpgp.domain.user.entity.User;
@@ -16,9 +18,6 @@ import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
-import javax.servlet.http.HttpSession;
-import java.util.Collections;
-
 /**
  * OAuth2 응답 처리를 위한 서비스
  */
@@ -31,42 +30,34 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
 
     private final HttpSession httpSession;
 
-    /**
-     * Provider에게 유저 정보를 반환하고, 세션에 유저 정보를 저장
-     *
-     * @param userRequest
-     * @return
-     * @throws OAuth2AuthenticationException
-     */
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2UserService delegate = new DefaultOAuth2UserService();
 
         OAuth2User oAuth2User = delegate.loadUser(userRequest);
 
-        //primary key를 의미, 구글은 기본 지원
-        String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails()
-            .getUserInfoEndpoint().getUserNameAttributeName();
 
         // OAuth2UserService를 통해 가져온 OAuth2User의 attribute를 담을 클래스
-        OAuthAttributes attributes = OAuthAttributes.of(userNameAttributeName,
-            oAuth2User.getAttributes());
+        OAuthAttributes attributes = OAuthAttributes.of(oAuth2User.getAttributes());
+
         User user = saveOrUpdate(attributes);
 
         //세션에 사용자 정보를 저장하기 위한 dto 클래스
         httpSession.setAttribute("user", new SessionUser(user));
 
         return new DefaultOAuth2User(
-            Collections.singleton(new SimpleGrantedAuthority(user.getRoleKey())),
-            attributes.getAttributes(),
-            attributes.getNameAttributeKey()
+                Collections.singleton(new SimpleGrantedAuthority(user.getRoleKey())),
+                attributes.getAttributes(),
+                attributes.getNameAttributeKey()
         );
     }
 
     private User saveOrUpdate(OAuthAttributes attributes) {
+
         User member = userRepository.findByEmail(attributes.getEmail())
-            .map(entity -> entity.updateEmail(attributes.getName()))
-            .orElse(attributes.toEntity());
+                .map(entity -> entity.updateName(attributes.getName()))
+                .orElse(attributes.toEntity());
+
 
         return userRepository.save(member);
     }
