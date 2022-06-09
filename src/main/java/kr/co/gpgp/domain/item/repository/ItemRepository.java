51ae89d1fb02baa658a45cefbdfee81d6f9c4897
kureungkureung <terry9611@naver.com>
2022-06-10 02:31:21 +0900

    refactor: 상품 중복 검사 메서드 수정

--- src/main/java/kr/co/gpgp/domain/item/repository/ItemRepository.java
+++ src/main/java/kr/co/gpgp/domain/item/repository/ItemRepository.java
@@ -8,5 +8,5 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
 
     Optional<Item> findByInfoCode(String code);
 
-    boolean existsByInfoCode(String code);
+    boolean existByInfoCode(String code);
 }
