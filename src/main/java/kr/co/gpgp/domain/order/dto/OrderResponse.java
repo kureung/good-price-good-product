9424d9dc70295e73d217c132c892072dc9049f63
kureungkureung <terry9611@naver.com>
2022-06-09 15:35:23 +0900

    refactor: 주문상품 응답 dto에 bean validation 제거

--- src/main/java/kr/co/gpgp/domain/order/dto/OrderResponse.java
+++ src/main/java/kr/co/gpgp/domain/order/dto/OrderResponse.java
@@ -4,13 +4,10 @@ import static lombok.AccessLevel.PRIVATE;
 
 import com.nimbusds.jose.shaded.json.annotate.JsonIgnore;
 import java.util.List;
-import javax.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import org.hibernate.validator.constraints.Length;
-import org.hibernate.validator.constraints.Range;
 
 @NoArgsConstructor
 @Getter
@@ -55,19 +52,12 @@ public class OrderResponse {
     @Builder
     public static class OrderLineResponse {
 
-        @NotBlank
-        @Length(max = 30)
         private final String itemName;
 
-        @NotBlank
-        @Length(max = 30)
         private final String itemCode;
 
-        @Range(min = 0, max = 1_000_000_000)
         private final int itemPrice;
 
-        @Range(min = 0, max = 10_000)
         private final int orderQuantity;
-
     }
 }
