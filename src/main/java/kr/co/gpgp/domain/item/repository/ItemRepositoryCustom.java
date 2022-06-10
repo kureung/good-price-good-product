72dd12b662a90a74015f3aea730dd1b64ecfbbff
kureungkureung <terry9611@naver.com>
2022-06-11 02:07:59 +0900

    feat: 상품 검색 기능

--- src/main/java/kr/co/gpgp/domain/item/repository/ItemRepositoryCustom.java
+++ src/main/java/kr/co/gpgp/domain/item/repository/ItemRepositoryCustom.java
@@ -1,4 +1,11 @@
 package kr.co.gpgp.domain.item.repository;
 
+import kr.co.gpgp.domain.item.dto.ItemResponse;
+import kr.co.gpgp.domain.item.search.ItemSearchCondition;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
+
 public interface ItemRepositoryCustom {
+    Page<ItemResponse> searchItem(ItemSearchCondition condition, Pageable pageable);
 }
--- src/main/java/kr/co/gpgp/domain/item/repository/ItemRepositoryCustomImpl.java
+++ src/main/java/kr/co/gpgp/domain/item/repository/ItemRepositoryCustomImpl.java
@@ -1,6 +1,75 @@
 package kr.co.gpgp.domain.item.repository;
 
+import static kr.co.gpgp.domain.item.entity.QItem.item;
+
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.List;
+import kr.co.gpgp.domain.item.dto.ItemResponse;
+import kr.co.gpgp.domain.item.dto.QItemResponse;
+import kr.co.gpgp.domain.item.search.ItemSearchCondition;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
+import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
+
+@Repository
+@RequiredArgsConstructor
 public class ItemRepositoryCustomImpl implements ItemRepositoryCustom {
 
+    private final JPAQueryFactory queryFactory;
 
+    @Override
+    public Page<ItemResponse> searchItem(ItemSearchCondition condition, Pageable pageable) {
+        List<ItemResponse> content = searchItemContent(condition, pageable);
+        JPAQuery<Long> totalCount = getTotalCount(condition);
+
+        return PageableExecutionUtils.getPage(content, pageable, totalCount::fetchOne);
+    }
+
+    private JPAQuery<Long> getTotalCount(ItemSearchCondition condition) {
+        return queryFactory
+                .select(item.count())
+                .from(item)
+                .where(
+                        itemNameContains(condition.getItemName()),
+                        priceGoe(condition.getPriceGoe()),
+                        priceLoe(condition.getPriceLoe()));
+    }
+
+    private List<ItemResponse> searchItemContent(ItemSearchCondition condition, Pageable pageable) {
+        return queryFactory
+                .select(new QItemResponse(
+                        item.id,
+                        item.price,
+                        item.stockQuantity,
+                        item.info.name,
+                        item.info.weight,
+                        item.info.code,
+                        item.info.imageUrl,
+                        item.info.releaseDate))
+                .from(item)
+                .where(
+                        itemNameContains(condition.getItemName()),
+                        priceGoe(condition.getPriceGoe()),
+                        priceLoe(condition.getPriceLoe()))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+    }
+
+    private BooleanExpression itemNameContains(String itemName) {
+        return StringUtils.hasText(itemName) ? item.info.name.contains(itemName):null;
+    }
+
+    private BooleanExpression priceGoe(Integer priceGoe) {
+        return priceGoe!=null ? item.price.goe(priceGoe):null;
+    }
+
+    private BooleanExpression priceLoe(Integer priceLoe) {
+        return priceLoe!=null ? item.price.loe(priceLoe):null;
+    }
 }
