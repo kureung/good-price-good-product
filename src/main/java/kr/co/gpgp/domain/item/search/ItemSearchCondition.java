1bb0fae31dd96198ed467428ee6ebd628cf128e1
kureung <89060425+kureung@users.noreply.github.com>
2022-06-11 02:13:25 +0900

    상품 검색 기능 추가 (#125)
    * feat: 사용자 정의 상품 리포지토리 생성
    
    * feat: 상품 검색 조건
    
    * feat: JPAQueryFactory 빈 등록
    
    * refactor: 상품 검색 기준 수정
    
    * feat: 상품 응답 dto에 쿼리프로젝션 추가
    
    * refactor: 주문 검색 조건 수정
    
    * feat: 상품 검색 기능
    
    * feat: 상품 검색 기능 테스트

---
+++ src/main/java/kr/co/gpgp/domain/item/search/ItemSearchCondition.java
@@ -0,0 +1,18 @@
+package kr.co.gpgp.domain.item.search;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class ItemSearchCondition {
+    private String itemName;
+    private Integer priceGoe;
+    private Integer priceLoe;
+
+    @Builder
+    private ItemSearchCondition(String itemName, Integer priceGoe, Integer priceLoe) {
+        this.itemName = itemName;
+        this.priceGoe = priceGoe;
+        this.priceLoe = priceLoe;
+    }
+}
