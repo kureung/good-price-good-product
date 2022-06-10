5d37ef228c7d50912a47b73d26636b90ac6c4308
kureungkureung <terry9611@naver.com>
2022-06-10 23:37:11 +0900

    feat: JPAQueryFactory 빈 등록

---
+++ src/main/java/kr/co/gpgp/auth/config/QuerydslConfig.java
@@ -0,0 +1,18 @@
+package kr.co.gpgp.auth.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import javax.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class QuerydslConfig {
+    @Autowired
+    private EntityManager em;
+
+    @Bean
+    public JPAQueryFactory queryFactory(EntityManager em) {
+        return new JPAQueryFactory(em);
+    }
+}
