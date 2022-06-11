b3aa50c8b242a0e6d48209dd7fa0ab80cdca16bb
kureungkureung <terry9611@naver.com>
2022-06-12 04:12:01 +0900

    feat: 주문 검색 기능 리포지토리 생성

--- src/main/java/kr/co/gpgp/domain/order/OrderRepository.java
+++ src/main/java/kr/co/gpgp/domain/order/OrderRepository.java
@@ -1,5 +1,11 @@
 package kr.co.gpgp.domain.order;
 
+import kr.co.gpgp.domain.order.entity.Order;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface OrderRepository {
+
+    Page<Order> orderSearch(OrderSearchCondition condition, Pageable pageable);
 
 }
--- src/main/java/kr/co/gpgp/repository/order/OrderRepositoryImpl.java
+++ src/main/java/kr/co/gpgp/repository/order/OrderRepositoryImpl.java
@@ -1,7 +1,11 @@
 package kr.co.gpgp.repository.order;
 
 import kr.co.gpgp.domain.order.OrderRepository;
+import kr.co.gpgp.domain.order.OrderSearchCondition;
+import kr.co.gpgp.domain.order.entity.Order;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -10,4 +14,10 @@ public class OrderRepositoryImpl implements OrderRepository {
 
     private final OrderJpaRepository jpaRepository;
     private final OrderRepositoryCustom repositoryCustom;
+
+    @Override
+    public Page<Order> orderSearch(OrderSearchCondition condition, Pageable pageable) {
+        return repositoryCustom.orderSearch(condition, pageable);
+    }
+
 }
