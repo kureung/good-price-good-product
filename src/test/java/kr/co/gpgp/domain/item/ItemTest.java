6d3f1fac0d1e8f2ad7802c00c2ddad7a82aa79e6
kureung <89060425+kureung@users.noreply.github.com>
2022-06-12 04:26:47 +0900

    주문 / 상품 테스트 패키지 수정 (#133)
    * refactor: 상품 테스트 패키지 이동
    
    * refactor: 상품 리포지토리 테스트 패키지 이동
    
    * refactor: 주문 리포지토리 테스트 패키지 이동

--- src/test/java/kr/co/gpgp/domain/item/ItemTest.java
+++ src/test/java/kr/co/gpgp/domain/item/ItemTest.java
@@ -3,6 +3,7 @@ package kr.co.gpgp.domain.item;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import kr.co.gpgp.domain.item.Item;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
