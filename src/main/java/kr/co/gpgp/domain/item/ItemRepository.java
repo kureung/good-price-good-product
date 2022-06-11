e778b17645872797e480de6c357cde0def420d06
kureungkureung <terry9611@naver.com>
2022-06-12 03:57:45 +0900

    refactor: 상품 jpa 리포지토리 수정

--- src/main/java/kr/co/gpgp/domain/item/ItemRepository.java
+++ src/main/java/kr/co/gpgp/domain/item/ItemRepository.java
@@ -1,5 +1,12 @@
 package kr.co.gpgp.domain.item;
 
+import java.util.Optional;
+import kr.co.gpgp.domain.item.entity.Item;
+
 public interface ItemRepository {
+
+    Optional<Item> findByInfoCode(String code);
+
+    boolean existsByInfoCode(String code);
 
 }
--- src/main/java/kr/co/gpgp/repository/item/ItemRepositoryImpl.java
+++ src/main/java/kr/co/gpgp/repository/item/ItemRepositoryImpl.java
@@ -1,6 +1,8 @@
 package kr.co.gpgp.repository.item;
 
+import java.util.Optional;
 import kr.co.gpgp.domain.item.ItemRepository;
+import kr.co.gpgp.domain.item.entity.Item;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +12,15 @@ public class ItemRepositoryImpl implements ItemRepository {
 
     private final ItemJpaRepository jpaRepository;
     private final ItemRepositoryCustom repositoryCustom;
+
+    @Override
+    public Optional<Item> findByInfoCode(String code) {
+        return jpaRepository.findByInfoCode(code);
+    }
+
+    @Override
+    public boolean existsByInfoCode(String code) {
+        return jpaRepository.existsByInfoCode(code);
+    }
 
 }
