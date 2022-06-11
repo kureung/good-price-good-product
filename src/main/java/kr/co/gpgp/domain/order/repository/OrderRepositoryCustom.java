94370c20ceb3645da479af2e92f9ef03c94a40d2
kureungkureung <terry9611@naver.com>
2022-06-12 00:48:52 +0900

    feat: 주문 검색 기능 인터페이스

---
+++ src/main/java/kr/co/gpgp/domain/order/repository/OrderRepositoryCustom.java
@@ -0,0 +1,10 @@
+package kr.co.gpgp.domain.order.repository;
+
+import kr.co.gpgp.domain.order.OrderSearchCondition;
+import kr.co.gpgp.domain.order.entity.Order;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface OrderRepositoryCustom {
+    Page<Order> orderSearch(OrderSearchCondition condition, Pageable pageable);
+}
