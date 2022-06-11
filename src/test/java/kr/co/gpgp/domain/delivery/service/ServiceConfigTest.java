2122ad02d08598ad49212536d3f95019ff614856
oiNeh <kgh22522252@gmail.com>
2022-06-12 03:30:51 +0900

    refactor: 코드 정리  (#123)

--- src/test/java/kr/co/gpgp/domain/delivery/service/ServiceConfigTest.java
+++ src/test/java/kr/co/gpgp/domain/delivery/service/ServiceConfigTest.java
@@ -4,18 +4,14 @@ import kr.co.gpgp.domain.address.repository.AddressRepository;
 import kr.co.gpgp.domain.delivery.repository.DeliveryRepository;
 import kr.co.gpgp.domain.user.repository.UserRepository;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.mock.mockito.MockBeans;
 import org.springframework.boot.test.mock.mockito.SpyBean;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(MockitoExtension.class)
 @DataJpaTest
-public class DeliveryServiceTest {
+public class ServiceConfigTest {
 
 
     @SpyBean
