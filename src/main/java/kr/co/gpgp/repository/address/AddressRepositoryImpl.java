b5451ad78af12df92bcd4e38869e2a04fa8f3e36
oiNeh <kgh22522252@gmail.com>
2022-06-13 11:58:37 +0900

    refactor: repositoryImpl 리턴 타입 변경 (#145)

--- src/main/java/kr/co/gpgp/repository/address/AddressRepositoryImpl.java
+++ src/main/java/kr/co/gpgp/repository/address/AddressRepositoryImpl.java
@@ -3,19 +3,18 @@ package kr.co.gpgp.repository.address;
 import java.util.List;
 import java.util.Optional;
 import kr.co.gpgp.domain.address.Address;
-import kr.co.gpgp.domain.address.dto.AddressResponse;
 import kr.co.gpgp.domain.address.AddressRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @RequiredArgsConstructor
-public class AddressRepositoryImpl  implements AddressRepository {
+public class AddressRepositoryImpl implements AddressRepository {
 
     private final AddressJpaRepository jpaRepository;
 
     @Override
-    public List<AddressResponse> findByUserId(Long userId) {
+    public List<Address> findByUserId(Long userId) {
         return jpaRepository.findByUserId(userId);
     }
 
