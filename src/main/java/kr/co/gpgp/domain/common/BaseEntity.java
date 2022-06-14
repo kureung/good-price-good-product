4591f0a74f996b09b0147a588eaffc39dcbd0418
kureungkureung <terry9611@naver.com>
2022-06-14 15:22:33 +0900

    feat: 등록자, 수정자 공통 속성

---
+++ src/main/java/kr/co/gpgp/domain/common/BaseEntity.java
@@ -0,0 +1,23 @@
+package kr.co.gpgp.domain.common;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity extends  BaseTimeEntity{
+
+    @CreatedBy
+    @Column(updatable = false)
+    private String createdBy;
+
+    @LastModifiedBy
+    private String lastModifiedBy;
+
+}
