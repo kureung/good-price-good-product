0f02d8b57b4123a679c2fbd231c9dade1036a8df
kureungkureung <terry9611@naver.com>
2022-06-14 15:01:14 +0900

    feat: enableJpaAuditing 등록

--- src/main/java/kr/co/gpgp/GpgpApplication.java
+++ src/main/java/kr/co/gpgp/GpgpApplication.java
@@ -3,9 +3,11 @@ package kr.co.gpgp;
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableEncryptableProperties
+@EnableJpaAuditing
 @SpringBootApplication
+@EnableEncryptableProperties
 public class GpgpApplication {
 
     public static void main(String[] args) {
