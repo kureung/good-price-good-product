190b6970db66f05c36d470fae47d986def0fa34d
oiNeh <kgh22522252@gmail.com>
2022-06-15 23:27:47 +0900

    feat: 배송 request 추가 (#160)

--- src/main/java/kr/co/gpgp/web/api/delivery/DeliveryRequest.java
+++ src/main/java/kr/co/gpgp/web/api/delivery/DeliveryRequest.java
@@ -1,8 +1,9 @@
-package kr.co.gpgp.domain.delivery.dto;
+package kr.co.gpgp.web.api.delivery;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import kr.co.gpgp.domain.address.Address;
+import kr.co.gpgp.domain.address.dto.AddressRequest;
 import kr.co.gpgp.domain.delivery.Delivery;
 import kr.co.gpgp.domain.requirement.Requirement;
 import lombok.AccessLevel;
@@ -12,6 +13,9 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class DeliveryRequest {
+
+    @NotNull(message = "배송 ID 값은 필수 입니다.")
+    private Long id;
 
     @Size(max = 18, min = 0, message = "요구사항 다시 확인해주세요.")
     private String requirement;
@@ -27,7 +31,9 @@ public class DeliveryRequest {
     @Size(max = 17, min = 0, message = "자세한주소를 다시 확인해주세요.")
     private String detailedAddress;
 
-    private DeliveryRequest(String requirement, String roadName, String zipCode, String addressName, String detailedAddress) {
+
+    private DeliveryRequest(Long id, String requirement, String roadName, String zipCode, String addressName, String detailedAddress) {
+        this.id = id;
         this.requirement = requirement;
         this.roadName = roadName;
         this.zipCode = zipCode;
@@ -35,12 +41,17 @@ public class DeliveryRequest {
         this.detailedAddress = detailedAddress;
     }
 
-    private static DeliveryRequest of(String requirement, String roadName, String zipCode, String addressName, String detailedAddress) {
-        return new DeliveryRequest(requirement, roadName, zipCode, addressName, detailedAddress);
-    }
-
-    public Delivery toEntity(Requirement requirement, Address address) {
-        return Delivery.of(requirement, address);
+    public Delivery toEntity() {
+        return Delivery.of(new Requirement(requirement),
+                Address.of(
+                        AddressRequest.of(
+                                getAddressName(),
+                                getZipCode(),
+                                getRoadName(),
+                                getDetailedAddress()
+                        )
+                )
+        );
     }
 
 }
