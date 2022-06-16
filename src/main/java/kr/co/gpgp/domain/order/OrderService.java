04e51ef7805829ab54600e4433080db0edc87370
kureungkureung <terry9611@naver.com>
2022-06-17 02:46:11 +0900

    refactor: 상품 서비스에 dto 의존성 끊어내기

--- src/main/java/kr/co/gpgp/domain/order/OrderService.java
+++ src/main/java/kr/co/gpgp/domain/order/OrderService.java
@@ -1,21 +1,12 @@
 package kr.co.gpgp.domain.order;
 
-import static java.util.stream.Collectors.toList;
-
 import java.util.List;
 import java.util.NoSuchElementException;
-import kr.co.gpgp.domain.address.Address;
 import kr.co.gpgp.domain.delivery.Delivery;
-import kr.co.gpgp.domain.item.Item;
 import kr.co.gpgp.domain.item.ItemFindService;
 import kr.co.gpgp.domain.orderline.OrderLine;
-import kr.co.gpgp.domain.requirement.Requirement;
 import kr.co.gpgp.domain.user.User;
 import kr.co.gpgp.domain.user.UserService;
-import kr.co.gpgp.web.api.order.OrderRequest;
-import kr.co.gpgp.web.api.order.OrderRequest.OrderLineRequest;
-import kr.co.gpgp.web.api.order.OrderResponse;
-import kr.co.gpgp.web.api.order.OrderResponse.OrderLineResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -32,43 +23,15 @@ public class OrderService {
     private final ItemFindService itemFindService;
 
     @Transactional
-    public Long order(Long userId, Delivery delivery, List<OrderLineRequest> orderLineRequests) {
-        if (orderLineRequests.isEmpty()) {
+    public Long order(Long userId, Delivery delivery, List<OrderLine> orderLines) {
+        if (orderLines.isEmpty()) {
             throw new IllegalArgumentException("주문하려는 상품을 입력해주세요.");
         }
 
         User user = userService.findOne(userId);
-        List<OrderLine> orderLines = this.toEntities(orderLineRequests);
 
         Order order = Order.of(user, delivery, orderLines);
         return orderRepository.save(order).getId();
-    }
-
-    @Transactional
-    public OrderResponse register(Long userId, OrderRequest request) {
-        User user = userService.findOne(userId);
-        Address address = Address.of(
-                user,
-                request.getRoadName(),
-                request.getZipCode(),
-                request.getAddressName(),
-                request.getDetailedAddress());
-
-        Requirement requirement = new Requirement(request.getRequirement());
-        Delivery delivery = Delivery.of(requirement, address);
-
-        Long orderId = order(userId, delivery, request.getOrderLines());
-        Order order = findOne(orderId);
-
-        return OrderResponse.builder()
-                .id(orderId)
-                .roadName(order.getAddressRoadName())
-                .requirement(order.getRequirementMessage())
-                .detailedAddress(order.getDetailedAddress())
-                .zipCode(order.getAddressZipCode())
-                .addressName(order.getAddressName())
-                .orderLines(toDtos(order.getOrderLines()))
-                .build();
     }
 
     @Transactional
@@ -85,51 +48,9 @@ public class OrderService {
                 .orElseThrow(() -> new NoSuchElementException("해당 주문을 찾을 수 없습니다."));
     }
 
-    public OrderResponse findOneToDto(Long orderId) {
-        Order order = findOne(orderId);
-
-        return OrderResponse.builder()
-                .id(orderId)
-                .roadName(order.getAddressRoadName())
-                .requirement(order.getRequirementMessage())
-                .detailedAddress(order.getDetailedAddress())
-                .zipCode(order.getAddressZipCode())
-                .addressName(order.getAddressName())
-                .orderLines(toDtos(order.getOrderLines()))
-                .build();
-
-    }
-
     public Page<OrderSearchResponse> searchOrder(OrderSearchCondition condition, Pageable pageable) {
         return orderRepository.orderSearch(condition, pageable);
 
-    }
-
-    public List<OrderLine> toEntities(List<OrderLineRequest> requests) {
-        return requests.stream()
-                .map(this::toEntity)
-                .collect(toList());
-    }
-
-    private OrderLine toEntity(OrderLineRequest request) {
-        String itemCode = request.getItemCode();
-        Item findItem = itemFindService.findOne(itemCode);
-        return OrderLine.of(findItem, request.getOrderQuantity());
-    }
-
-    public List<OrderLineResponse> toDtos(List<OrderLine> orderLines) {
-        return orderLines.stream()
-                .map(this::toDto)
-                .collect(toList());
-    }
-
-    private OrderLineResponse toDto(OrderLine orderLine) {
-        return OrderLineResponse.builder()
-                .itemCode(orderLine.getItemCode())
-                .itemPrice(orderLine.getPrice())
-                .orderQuantity(orderLine.getOrderQuantity())
-                .itemName(orderLine.getItemName())
-                .build();
     }
 
 }
