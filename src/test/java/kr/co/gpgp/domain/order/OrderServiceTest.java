3eb5325f95d557dd493eb9ecee6a04ffdbab0263
kureungkureung <terry9611@naver.com>
2022-06-17 01:49:18 +0900

    feat: 주문 검색 테스트 구현

--- src/test/java/kr/co/gpgp/domain/order/OrderServiceTest.java
+++ src/test/java/kr/co/gpgp/domain/order/OrderServiceTest.java
@@ -1,5 +1,6 @@
 package kr.co.gpgp.domain.order;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
@@ -7,21 +8,26 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.util.List;
 import kr.co.gpgp.domain.address.Address;
+import kr.co.gpgp.domain.address.AddressRepository;
 import kr.co.gpgp.domain.delivery.Delivery;
+import kr.co.gpgp.domain.delivery.DeliveryRepository;
 import kr.co.gpgp.domain.item.Item;
 import kr.co.gpgp.domain.item.ItemInfo;
-import kr.co.gpgp.web.api.order.OrderRequest.OrderLineRequest;
-import kr.co.gpgp.web.api.order.OrderResponse.OrderLineResponse;
 import kr.co.gpgp.domain.orderline.OrderLine;
 import kr.co.gpgp.domain.requirement.Requirement;
+import kr.co.gpgp.domain.requirement.RequirementRepository;
 import kr.co.gpgp.domain.user.Role;
 import kr.co.gpgp.domain.user.User;
 import kr.co.gpgp.domain.user.UserRepository;
 import kr.co.gpgp.repository.item.ItemJpaRepository;
+import kr.co.gpgp.web.api.order.OrderRequest.OrderLineRequest;
+import kr.co.gpgp.web.api.order.OrderResponse.OrderLineResponse;
 import kr.co.gpgp.web.exception.ErrorCode;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
@@ -36,6 +42,18 @@ class OrderServiceTest {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private AddressRepository addressRepository;
+
+    @Autowired
+    private DeliveryRepository deliveryRepository;
+
+    @Autowired
+    private RequirementRepository requirementRepository;
+
+    @Autowired
+    private OrderRepository orderRepository;
 
     @Test
     void 상품_주문_테스트() {
@@ -232,6 +250,55 @@ class OrderServiceTest {
         List<OrderLine> responses = assertDoesNotThrow(() -> sut.toEntities(requests));
         assertNotNull(responses);
         assertInstanceOf(List.class, responses);
+    }
+
+    @Test
+    void 주문_검색_테스트() {
+        // given
+        OrderSearchCondition condition = OrderSearchCondition.builder()
+                .userId(1L)
+                .build();
+
+        User user = User.of("name1", "abc@naver.com", Role.USER);
+        userRepository.save(user);
+
+        Requirement requirement = new Requirement("요청사항");
+        requirementRepository.save(requirement);
+
+        Address address = Address.of(user, "123456789", "12345", "name1", "detailed");
+        addressRepository.save(address);
+
+        Delivery delivery = Delivery.of(requirement, address);
+        deliveryRepository.save(delivery);
+
+        for (int i = 0; i < 100; i++) {
+            ItemInfo info = ItemInfo.builder()
+                    .code("123")
+                    .weight(20)
+                    .build();
+
+            Item item = Item.builder()
+                    .info(info)
+                    .price(1000)
+                    .stockQuantity(100)
+                    .build();
+            itemJpaRepository.save(item);
+
+            OrderLine orderLine = OrderLine.of(item, i);
+            List<OrderLine> orderLines = List.of(orderLine);
+
+            Order order = Order.of(user, delivery, orderLines);
+            orderRepository.save(order);
+        }
+
+        // when
+        int size = 5;
+        PageRequest pageRequest = PageRequest.of(0, size);
+        Page<OrderSearchResponse> searchResponses = sut.searchOrder(condition, pageRequest);
+        List<OrderSearchResponse> content = searchResponses.getContent();
+
+        // then
+        assertThat(content.size()).isEqualTo(size);
     }
 
 }
