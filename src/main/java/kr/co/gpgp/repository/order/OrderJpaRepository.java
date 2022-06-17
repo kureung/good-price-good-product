cf6c5e3383cd4e78fe0e273c308285e652d19a36
Kang JeoungHun <105915960+oiNeh@users.noreply.github.com>
2022-06-18 03:08:35 +0900

    fix: orderjpa 수정 (#176)

--- src/main/java/kr/co/gpgp/repository/order/OrderJpaRepository.java
+++ src/main/java/kr/co/gpgp/repository/order/OrderJpaRepository.java
@@ -4,6 +4,8 @@ import java.util.List;
 import kr.co.gpgp.domain.order.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderJpaRepository extends JpaRepository<Order, Long> {
+public interface OrderJpaRepository extends JpaRepository<Order, Long>, OrderRepositoryCustom {
+
     List<Order> findByUserId(Long userId);
+
 }
