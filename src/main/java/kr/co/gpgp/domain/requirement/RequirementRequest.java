35342dfe185932bdeab893de0a68a07ff867e94c
oiNeh <kgh22522252@gmail.com>
2022-06-18 14:29:28 +0900

    feat: 요청사항 req 수정 (#171)

--- src/main/java/kr/co/gpgp/domain/requirement/RequirementRequest.java
+++ src/main/java/kr/co/gpgp/domain/requirement/RequirementRequest.java
@@ -3,17 +3,21 @@ package kr.co.gpgp.domain.requirement;
 
 public class RequirementRequest {
 
+    private Long id;
     private String message;
 
-    private RequirementRequest(String message) {
+    private RequirementRequest(Long id, String message) {
+        this.id=id;
         this.message = message;
     }
 
-    public static RequirementRequest of(String message) {
-        return new RequirementRequest(message);
+    public static RequirementRequest of(Long id, String message) {
+        return new RequirementRequest(id,message);
     }
 
     public String getMessage() {
         return message;
     }
+
+    public Long getId() { return id; }
 }
