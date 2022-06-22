2bbdd26f763f28717afe8d884cf4a5223c3e13ba
oiNeh <kgh22522252@gmail.com>
2022-06-22 16:08:51 +0900

    feat: oauth2 config 설정 변경 (#199)

--- src/main/java/kr/co/gpgp/auth/config/SecurityConfig.java
+++ src/main/java/kr/co/gpgp/auth/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package kr.co.gpgp.config;
+package kr.co.gpgp.auth.config;
 
 import kr.co.gpgp.auth.service.CustomOAuth2UserService;
 import kr.co.gpgp.domain.user.Role;
@@ -6,12 +6,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.access.AccessDeniedHandler;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final CustomOAuth2UserService customOAuth2UserService;
+    private final  AccessDeniedHandler CustomAccessDeniedHandler;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -23,10 +25,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/", "/css/**", "/img/**", "/templates/img/*.png", "/js/**", "/h2-console/**", "/api/**")
                 .permitAll()
                 .antMatchers("/user").hasRole(Role.USER.name())
-                .anyRequest().authenticated().and() //anyRequest 나머지 url들을 나타냄, authenticated 인증된 경우만 허용
-
-                .logout().logoutSuccessUrl("/").and() //로그아웃 기능 설정 진입점, 로그아웃 성공 시 /로 이동
+                .anyRequest().authenticated() //anyRequest 나머지 url들을 나타냄, authenticated 인증된 경우만 허용
+                .and()
+                .exceptionHandling()
+                .accessDeniedHandler(CustomAccessDeniedHandler)// 권한이 없을 경우 저희가 custom 으로 만든 handler 를 추가
+                .and()
+                .logout().logoutSuccessUrl("/") //로그아웃 기능 설정 진입점, 로그아웃 성공 시 /로 이동
+                .and()
                 .oauth2Login()
+                .defaultSuccessUrl("/")//oauth2 인증 성공시 지정 경로
                 .userInfoEndpoint().userService(customOAuth2UserService); //로그인 기능 설정 진입점, 소셜 로그인 성공 시 후속 조치 진행 Service 인터페이스 구현체 등록
     }
 
