9d166b28dc35b29b87784b23ae194a9939ed3a22
oiNeh <kgh22522252@gmail.com>
2022-06-22 16:04:06 +0900

    feat: 주소 req @Setter 추가  (#199)

--- src/main/java/kr/co/gpgp/web/api/address/AddressRequest.java
+++ src/main/java/kr/co/gpgp/web/api/address/AddressRequest.java
@@ -6,8 +6,10 @@ import kr.co.gpgp.domain.address.Address;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class AddressRequest {
 
