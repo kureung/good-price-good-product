c99d765eb5648ef264771182e97872582a7c4cc1
oiNeh <kgh22522252@gmail.com>
2022-06-22 16:09:12 +0900

    feat: 유저 컨트롤 변경 (#199)

--- src/main/java/kr/co/gpgp/web/api/user/UserController.java
+++ src/main/java/kr/co/gpgp/web/api/user/UserController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/user")
+@RequestMapping("/user")
 public class UserController {
 
     private final UserService userService;
