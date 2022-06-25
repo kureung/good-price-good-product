d8ae24688a369423daecc7f64ce8727cd1df465c
oiNeh <kgh22522252@gmail.com>
2022-06-25 21:42:39 +0900

    feat: address api 수정(#201)

--- src/main/java/kr/co/gpgp/web/api/address/AddressApiController.java
+++ src/main/java/kr/co/gpgp/web/api/address/AddressApiController.java
@@ -3,21 +3,20 @@ package kr.co.gpgp.web.api.address;
 
 import java.util.List;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
-import kr.co.gpgp.auth.dto.SessionUser;
+import kr.co.gpgp.auth.dto.UserDetails;
 import kr.co.gpgp.domain.address.Address;
 import kr.co.gpgp.domain.address.AddressDto;
 import kr.co.gpgp.domain.address.AddressService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
-
 
 @RestController
 @RequiredArgsConstructor
@@ -31,58 +30,48 @@ public class AddressApiController {
             AddressRequest addressRequest,
             HttpServletRequest request
     ) {
-        Long userId = getSessionUserId(request);
+
+        UserDetails user = UserDetails.of(SecurityContextHolder.getContext().getAuthentication().getPrincipal());
 
         AddressDto addressDto = AddressDto.of(addressRequest.getId(), addressRequest.getId(), addressRequest.getRoadName(), addressRequest.getZipCode(), addressRequest.getName(), addressRequest.getDetailed());
 
-        Address address = addressService.create(userId,addressDto);
+        Address address = addressService.create(user.getId(), addressDto);
 
         return ResponseEntity.ok().body(address);
     }
 
     @DeleteMapping("/{addressId}")
     public ResponseEntity<Void> delete(
-            HttpServletRequest request,
             @Valid Long addressId
     ) {
-        Long userId = getSessionUserId(request);
-
         addressService.delete(addressId);
         return ResponseEntity.ok().build();
     }
 
     @PostMapping("/update")
     public ResponseEntity<Void> update(
-            HttpServletRequest request,
             AddressRequest addressRequest
     ) {
-
-        Long userId = getSessionUserId(request);
+        UserDetails user = UserDetails.of(SecurityContextHolder.getContext().getAuthentication().getPrincipal());
 
         AddressDto addressDto = AddressDto.of(addressRequest.getId(), addressRequest.getId(), addressRequest.getRoadName(), addressRequest.getZipCode(), addressRequest.getName(), addressRequest.getDetailed());
 
-        addressService.update(userId, addressDto);
+        addressService.update(user.getId(), addressDto);
 
         return ResponseEntity.ok().build();
     }
 
     @GetMapping("")
-    public ModelAndView home(
-            HttpServletRequest request
-    ) {
+    public ModelAndView home() {
         ModelAndView mav = new ModelAndView("address");
+        UserDetails user = UserDetails.of(SecurityContextHolder.getContext().getAuthentication().getPrincipal());
 
-        Long userId = getSessionUserId(request);
-        List<Address> address = addressService.select(userId);
-        List<AddressResponse> responses= AddressResponse.of(address);
-        mav.addObject("addressList",responses);
+        List<Address> address = addressService.select(user.getId());
+        List<AddressResponse> responses = AddressResponse.of(address);
+
+        mav.addObject("addressList", responses);
         return mav;
     }
 
 
-    private Long getSessionUserId(HttpServletRequest request) {
-        HttpSession session = request.getSession();
-        SessionUser user = (SessionUser) session.getAttribute("user");
-        return user.getId();
-    }
 }
