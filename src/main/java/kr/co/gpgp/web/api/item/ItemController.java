19fe5909fe40aef1a6bd3ee174d5e86b705a960f
kureungkureung <terry9611@naver.com>
2022-06-26 02:54:10 +0900

    feat: 상품 컨트롤러 생성

---
+++ src/main/java/kr/co/gpgp/web/api/item/ItemController.java
@@ -0,0 +1,10 @@
+package kr.co.gpgp.web.api.item;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/items")
+public class ItemController {
+
+}
