200e8334f2ff146afc4b36434484183be9873296
kureungkureung <terry9611@naver.com>
2022-06-26 02:57:28 +0900

    feat: 상품 등록 폼 조회 기능 구현

--- src/main/java/kr/co/gpgp/web/api/item/ItemController.java
+++ src/main/java/kr/co/gpgp/web/api/item/ItemController.java
@@ -1,10 +1,17 @@
 package kr.co.gpgp.web.api.item;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/items")
 public class ItemController {
+
+    @GetMapping("/new")
+    public String createForm(@ModelAttribute NewItemCreateForm form) {
+        return "admin/create-new-item-form";
+    }
 
 }
