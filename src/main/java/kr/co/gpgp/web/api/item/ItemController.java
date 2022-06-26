4f71c3fb8d04f4d0732027268e4a410a5860c700
kureung <89060425+kureung@users.noreply.github.com>
2022-06-26 12:26:20 +0900

    상품 등록 폼 컨트롤러 구현 (#210)
    * feat: 상품 컨트롤러 생성
    
    * feat: 상품 등록 폼 구현
    
    * feat: 상품 등록 폼 조회 기능 구현
    
    * feat: 상품 어댑터 구현
    
    * feat: 상품 등록 기능 구현

---
+++ src/main/java/kr/co/gpgp/web/api/item/ItemController.java
@@ -0,0 +1,39 @@
+package kr.co.gpgp.web.api.item;
+
+import javax.validation.Valid;
+import kr.co.gpgp.domain.item.Item;
+import kr.co.gpgp.domain.item.ItemCommandService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/items")
+public class ItemController {
+
+    private final ItemCommandService itemCommandService;
+
+    @GetMapping("/new")
+    public String createForm(@ModelAttribute NewItemCreateForm form) {
+        return "admin/create-new-item-form";
+    }
+
+    @PostMapping("/new")
+    public String create(@Valid @ModelAttribute NewItemCreateForm form,
+                         BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return "admin/create-new-item-form";
+        }
+
+        Item item = ItemAdapter.toEntity(form);
+        itemCommandService.save(item);
+        return "redirect:/";
+    }
+
+}
