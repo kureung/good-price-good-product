31d0abfbfec906fd127338c3163b18cf48375d14
Junnyjun <chbe5082@naver.com>
2022-06-29 01:24:42 +0900

    🍎feat(config): 불필요한 의존성 제거

--- src/main/java/kr/co/gpgp/config/QuerydslConfig.java
+++ src/main/java/kr/co/gpgp/config/QuerydslConfig.java
@@ -2,15 +2,15 @@ package kr.co.gpgp.config;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import javax.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class QuerydslConfig {
-
-    @Autowired
-    private EntityManager em;
+    private final EntityManager em;
 
     @Bean
     public JPAQueryFactory queryFactory(EntityManager em) {
