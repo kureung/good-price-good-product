eee2966e58efb7a2ee25968c55a81ee9e75a03f3
oiNeh <kgh22522252@gmail.com>
2022-06-29 23:07:46 +0900

    refactor: 유저 컨트롤에서 session 제거 (#213)

--- src/main/java/kr/co/gpgp/web/api/user/UserApiController.java
+++ src/main/java/kr/co/gpgp/web/api/user/UserApiController.java
@@ -2,23 +2,25 @@ package kr.co.gpgp.web.api.user;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import kr.co.gpgp.auth.dto.SessionUser;
+import kr.co.gpgp.auth.dto.UserDetails;
 import kr.co.gpgp.domain.user.User;
 import kr.co.gpgp.domain.user.UserRepository;
 import kr.co.gpgp.domain.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 @RequestMapping("/user")
 public class UserApiController {
     private final UserService userService;
-    private final UserRepository userRepository;
 
     //권한 변경
     @GetMapping("/role/{id}/{role}")
@@ -27,19 +29,21 @@ public class UserApiController {
             @PathVariable String role
     ) {
         userService.changeOfPermission(id, role);
-        User user = userRepository.findById(id).get();
-
 
         return ResponseEntity.ok().build();
     }
 
     @GetMapping("/")
-    public ResponseEntity<SessionUser> roleUpdate(
-            HttpServletRequest request
+    public ResponseEntity<Void> roleUpdate(
     ) {
-        HttpSession session = request.getSession();
-        SessionUser user = (SessionUser) session.getAttribute("user");
-
-        return ResponseEntity.ok().body(user );
+        return ResponseEntity.ok().build();
     }
+
+    @PutMapping
+    public String roleup(Authentication authentication){
+        UserDetails user = UserDetails.of(authentication.getPrincipal());
+
+        return "mypage";
+    }
+
 }
