ec20bb21b80391bbbe996592c2377c5d0691ec4b
oiNeh <kgh22522252@gmail.com>
2022-06-29 23:06:07 +0900

    feat: 요청사항 findByUserId 추가   (#213)

--- src/main/java/kr/co/gpgp/domain/requirement/RequirementRepository.java
+++ src/main/java/kr/co/gpgp/domain/requirement/RequirementRepository.java
@@ -1,10 +1,11 @@
 package kr.co.gpgp.domain.requirement;
 
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RequirementRepository extends JpaRepository<Requirement, Long> {
 
     Optional<Requirement> findByMessage(String message);
-
+    List<Requirement> findByUserId(Long userId);
 }
