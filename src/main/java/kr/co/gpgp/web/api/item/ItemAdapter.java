38e0045012f8959f6787bc05168274f732b98833
kureungkureung <terry9611@naver.com>
2022-06-30 01:11:28 +0900

    feat: 상품 등록폼이 엔티티로 변환 기능 추가

--- src/main/java/kr/co/gpgp/web/api/item/ItemAdapter.java
+++ src/main/java/kr/co/gpgp/web/api/item/ItemAdapter.java
@@ -22,4 +22,17 @@ public class ItemAdapter {
                 .build();
     }
 
+    public static Item toEntity(NewItemCreateForm form) {
+        ItemInfo info = ItemInfo.builder()
+                .author(form.getAuthor())
+                .name(form.getName())
+                .build();
+
+        return Item.builder()
+                .info(info)
+                .stockQuantity(form.getStockQuantity())
+                .price(form.getPrice())
+                .build();
+    }
+
 }
