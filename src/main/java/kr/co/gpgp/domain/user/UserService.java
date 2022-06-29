4dba743350eca7cc82ecfda630e599b6718e18b6
oiNeh <kgh22522252@gmail.com>
2022-06-30 04:04:36 +0900

    feat: 유저 서비스 수정(#229)

--- src/main/java/kr/co/gpgp/domain/user/UserService.java
+++ src/main/java/kr/co/gpgp/domain/user/UserService.java
@@ -15,6 +15,8 @@ public class UserService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 회원을 찾을 수 없습니다."));
     }
 
+
+    @Transactional
     public void changeOfPermission(Long userId, Role role) {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new IllegalArgumentException("권한 변경하려는 회원 을 찾을수 없습니다."));
