bab03c1a1bc59e0cfb2879256ceaad84fce6f238
kureung <89060425+kureung@users.noreply.github.com>
2022-07-03 02:24:14 +0900

    상품 검색 페이징 컨트롤러 구현 (#238)
    * feat: 페이징 처리 기능 클래스 구현
    
    * style: 필요없는 임포트 제거
    
    * feat: 페이징 컨트롤러 기능 구현
    
    * refactor: 상품 등록폼에 name 속성 추가
    
    * feat: 상품 페이징 뷰 구현
    
    * refactor: system.out.println 출력 제거
    
    * style: 불필요한 공백 제거
    
    * refactor: 안쓰는 script 제거

--- src/main/java/kr/co/gpgp/web/api/item/ItemController.java
+++ src/main/java/kr/co/gpgp/web/api/item/ItemController.java
@@ -1,14 +1,26 @@
 package kr.co.gpgp.web.api.item;
 
+import java.util.List;
+import javax.annotation.PostConstruct;
 import javax.validation.Valid;
 import kr.co.gpgp.auth.dto.UserDetails;
 import kr.co.gpgp.domain.item.Item;
 import kr.co.gpgp.domain.item.ItemCommandService;
+import kr.co.gpgp.domain.item.ItemFindService;
+import kr.co.gpgp.domain.item.ItemSearchCondition;
+import kr.co.gpgp.domain.item.ItemSearchCondition.ItemSearchConditionBuilder;
+import kr.co.gpgp.domain.item.ItemSearchDto;
 import kr.co.gpgp.domain.user.Seller;
 import kr.co.gpgp.domain.user.SellerService;
+import kr.co.gpgp.web.common.Pages;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -16,13 +28,16 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/items")
 public class ItemController {
 
     private final ItemCommandService itemCommandService;
+    private final ItemFindService itemFindService;
     private final SellerService sellerService;
 
     @GetMapping
@@ -31,10 +46,9 @@ public class ItemController {
     }
 
     @PostMapping
-    public String create(@Valid @ModelAttribute NewItemCreateForm form,
+    public String create(@Valid @ModelAttribute NewItemCreateForm newItemCreateForm,
                          BindingResult bindingResult
     ) {
-
         if (bindingResult.hasErrors()) {
             return "seller/create-new-item-form";
         }
@@ -43,9 +57,10 @@ public class ItemController {
 
         Seller seller = sellerService.findOneByUser(userId);
 
-        Item item = ItemAdapter.toEntity(form, seller);
+        Item item = ItemAdapter.toEntity(newItemCreateForm, seller);
+
         itemCommandService.save(item);
-        return "redirect:/";
+        return "redirect:/items";
     }
 
     @PatchMapping("/{itemId}")
@@ -53,7 +68,6 @@ public class ItemController {
                        @Valid @ModelAttribute NewItemCreateForm form,
                        BindingResult bindingResult
     ) {
-
         if (bindingResult.hasErrors()) {
             return "seller/create-new-item-form";
         }
@@ -62,6 +76,31 @@ public class ItemController {
         itemCommandService.update(itemId, item);
 
         return "redirect:/";
+    }
+
+    @GetMapping("/list")
+    public String searchItem(
+            @RequestParam(required = false) String itemName,
+            Pageable pageable,
+            Model model
+    ) {
+        ItemSearchCondition condition = ItemSearchCondition.builder()
+                .itemName(itemName)
+                .build();
+        PageRequest pageRequest = PageRequest.of(pageable.getPageNumber(), 10);
+        Page<ItemSearchDto> searchItems = itemFindService.search(condition, pageRequest);
+
+        model.addAttribute("searchItems", searchItems);
+
+        Pages<ItemSearchDto> pages = Pages.of(searchItems, 4);
+
+        model.addAttribute("pages", pages.getPages());
+
+        log.info("pages.getPages()= '{}'", pages.getPages());
+        log.info("firstPage='{}'", pages.getFirstPage());
+        log.info("lastPage='{}'", pages.getLastPage());
+
+        return "items/list";
     }
 
 }
