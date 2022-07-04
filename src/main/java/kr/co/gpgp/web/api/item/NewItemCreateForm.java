d7afaadcd90a249f5c172050a461a917799842df
kureungkureung <terry9611@naver.com>
2022-07-05 02:00:58 +0900

    feat: 상품 등록/수정 폼에 생성자 추가

--- src/main/java/kr/co/gpgp/web/api/item/NewItemCreateForm.java
+++ src/main/java/kr/co/gpgp/web/api/item/NewItemCreateForm.java
@@ -1,10 +1,12 @@
 package kr.co.gpgp.web.api.item;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class NewItemCreateForm {
 
     private String author = "";
