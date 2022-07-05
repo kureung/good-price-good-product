2fc5018f7d8834d128e7945b9d8e0eee42065931
oiNeh <kgh22522252@gmail.com>
2022-07-05 21:39:44 +0900

    feat: 배송원 repositoryImpl 추가 (#223)

---
+++ src/main/java/kr/co/gpgp/repository/courier/CourierRepositoryImpl.java
@@ -0,0 +1,32 @@
+package kr.co.gpgp.repository.courier;
+
+import java.util.List;
+import kr.co.gpgp.domain.courier.Courier;
+import kr.co.gpgp.domain.courier.CourierArea;
+import kr.co.gpgp.domain.courier.CourierRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class CourierRepositoryImpl implements CourierRepository {
+
+    private final CourierJpaRepository jpaRepository;
+
+    @Override
+    public void save(Courier courier) {
+        jpaRepository.save(courier);
+    }
+
+    @Override
+    public Courier findByUserId(Long id) {
+        return jpaRepository.findByUserId(id);
+    }
+
+    @Override
+    public List<Courier> findByCourierArea(CourierArea courierArea) {
+        return jpaRepository.findByCourierArea(courierArea);
+    }
+
+
+}
