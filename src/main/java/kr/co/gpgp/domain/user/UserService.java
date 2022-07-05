a9c659908d6d86765beddf49581b8a413fad3661
oiNeh <kgh22522252@gmail.com>
2022-07-05 21:42:25 +0900

    feat: 유저가 서비스 수정 (#223)
    유저가 배송원으로 권한을 변경시
    서비스 추가

--- src/main/java/kr/co/gpgp/domain/user/UserService.java
+++ src/main/java/kr/co/gpgp/domain/user/UserService.java
@@ -1,5 +1,8 @@
 package kr.co.gpgp.domain.user;
 
+import kr.co.gpgp.domain.courier.Courier;
+import kr.co.gpgp.domain.courier.CourierArea;
+import kr.co.gpgp.domain.courier.CourierRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,6 +13,7 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final SellerRepository sellerRepository;
+    private final CourierRepository courierRepository;
 
     public User findOne(Long userId) {
         return userRepository.findById(userId)
@@ -23,9 +27,26 @@ public class UserService {
                 .orElseThrow(() -> new IllegalArgumentException("권한 변경하려는 회원 을 찾을수 없습니다."));
 
         if (user.getRole() == Role.USER) {
-            if(role == Role.SELLER){
+            if (role == Role.SELLER) {
                 Seller seller = Seller.of(user);
                 sellerRepository.save(seller);
+            }
+
+
+        }
+
+        user.updateRole(role);
+    }
+
+    @Transactional
+    public void changeOfPermission(Long userId, Role role, CourierArea courierArea) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("권한 변경하려는 회원 을 찾을수 없습니다."));
+
+        if (user.getRole() == Role.USER) {
+            if(role == Role.COURIER){
+                Courier courier = Courier.of(user,courierArea);
+                courierRepository.save(courier);
             }
         }
 
