d1fcb809f7a501b5d81a33afbea9cbe1ac0aaadb
oiNeh <kgh22522252@gmail.com>
2022-07-05 21:37:23 +0900

    feat: 배송원 컨테이너 jpaRepository 추가 (#223)

---
+++ src/main/java/kr/co/gpgp/repository/courier/CourierContainerJpaRepository.java
@@ -0,0 +1,10 @@
+package kr.co.gpgp.repository.courier;
+
+import java.util.List;
+import kr.co.gpgp.domain.courier.Courier;
+import kr.co.gpgp.domain.courier.CourierContainer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CourierContainerJpaRepository extends JpaRepository<CourierContainer, Long> {
+    List<CourierContainer> findByCourier(Courier courier);
+}
