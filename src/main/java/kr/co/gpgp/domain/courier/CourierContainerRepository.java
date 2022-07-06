48769b7d8060f90e7c4f878b98df8f6e8b13d49d
oiNeh <kgh22522252@gmail.com>
2022-07-06 22:52:36 +0900

    style: 배송원 컨테이너 repository

--- src/main/java/kr/co/gpgp/domain/courier/CourierContainerRepository.java
+++ src/main/java/kr/co/gpgp/domain/courier/CourierContainerRepository.java
@@ -1,11 +1,14 @@
 package kr.co.gpgp.domain.courier;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CourierContainerRepository {
 
     void save(CourierContainer courierContainer);
 
-    List<CourierContainer> findByCourier(Courier courier);
+    List<CourierContainer> findByCourierList(Courier courier);
+
+    Optional<CourierContainer> findById(Long id);
 
 }
