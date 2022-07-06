0fe996730cf1d6bf495cecdb79d77939041d01cb
oiNeh <kgh22522252@gmail.com>
2022-07-06 22:54:46 +0900

    refactor: 배송원 상태 수정

--- src/main/java/kr/co/gpgp/domain/courier/CourierStatus.java
+++ src/main/java/kr/co/gpgp/domain/courier/CourierStatus.java
@@ -1,12 +1,8 @@
 package kr.co.gpgp.domain.courier;
 
 public enum CourierStatus {
-    /** 배송대기 상태 */
     WAITING_FOR_DELIVERY("배송대기"),
-    /** 배송중 상태 */
-    shipping("배송중"),
-
-    /** 배송완료 상태 */
+    SHIPPING("배송중"),
     COMPLETION("배송완료");
 
     private String message;
@@ -21,16 +17,6 @@ public enum CourierStatus {
 
     private String getMessage() {
         return message;
-    }
-
-    CourierStatus stateChange() {
-        if (this == WAITING_FOR_DELIVERY) {
-            return shipping;
-        }
-        return WAITING_FOR_DELIVERY;
-    }
-    CourierStatus completion() {
-        return COMPLETION;
     }
 
 }
