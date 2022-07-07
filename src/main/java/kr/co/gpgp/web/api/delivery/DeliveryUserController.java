7cc50a78bf7a723f07fcf4719949ea1f1e27d40a
oiNeh <kgh22522252@gmail.com>
2022-07-07 20:43:32 +0900

    refactor: 배송 유저 컨트롤 수정

--- src/main/java/kr/co/gpgp/web/api/delivery/DeliveryUserController.java
+++ src/main/java/kr/co/gpgp/web/api/delivery/DeliveryUserController.java
@@ -3,36 +3,32 @@ package kr.co.gpgp.web.api.delivery;
 import java.util.List;
 import javax.validation.Valid;
 import kr.co.gpgp.auth.dto.UserDetails;
-import kr.co.gpgp.domain.delivery.Delivery;
 import kr.co.gpgp.domain.delivery.DeliveryUserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/user/delivery")
 public class DeliveryUserController {
-    private final DeliveryUserService deliveryUserService;
 
+    private final DeliveryUserService deliveryUserService;
+    private final DeliveryDtoFacade deliveryDtoFacade;
 
     @GetMapping
     public String get(Model model, Authentication authentication) {
         UserDetails user = UserDetails.of(authentication.getPrincipal());
 
-        List<Delivery> delivery = deliveryUserService.selectAll(user.getId());
-        List<DeliveryResponse> deliveryResponse = DeliveryResponse.convertList(delivery);
+        List<DeliveryResponse> deliveryResponse = deliveryDtoFacade.convertResponse
+                (deliveryUserService.selectAll(user.getId()));
 
-        //주문정보
-//        model.addAttribute("order",orderResponse);
-        //배송정보
-        model.addAttribute("delivery",deliveryResponse);
+        model.addAttribute("delivery", deliveryResponse);
+
         return "/users/delivery";
     }
 
@@ -40,10 +36,11 @@ public class DeliveryUserController {
     public String update(
             @Valid Long deliveryId,
             Authentication authentication
-    ){
+    ) {
         UserDetails user = UserDetails.of(authentication.getPrincipal());
 
-        deliveryUserService.update(user.getId(),deliveryId);
+        deliveryUserService.update(user.getId(), deliveryId);
+
         return "redirect:/users/delivery";
     }
 
