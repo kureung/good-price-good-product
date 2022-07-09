a730ec5118360b8f6cc228ca419493ecc0489e4a
oiNeh <kgh22522252@gmail.com>
2022-07-10 01:53:31 +0900

    refactor: 배송원 jpaRepository 수정

--- src/main/java/kr/co/gpgp/repository/courier/CourierJpaRepository.java
+++ src/main/java/kr/co/gpgp/repository/courier/CourierJpaRepository.java
@@ -1,13 +1,14 @@
 package kr.co.gpgp.repository.courier;
 
 import java.util.List;
+import java.util.Optional;
 import kr.co.gpgp.domain.courier.Courier;
 import kr.co.gpgp.domain.courier.CourierArea;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CourierJpaRepository extends JpaRepository<Courier, Long> {
 
-    Courier findByUserId(Long id);
+    Optional<Courier> findByUserId(Long id);
 
     List<Courier> findByCourierArea(CourierArea courierArea);
 
